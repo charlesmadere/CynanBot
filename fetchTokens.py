d04f11dfe98cafe380aa29696e054a9f03a34a4c
Charles Madere <charlesrmadere@gmail.com>
2020-07-02 19:20:44 -0700

    cleaned up some of the client secrets

--- fetchTokens.py
+++ fetchTokens.py
@@ -1,13 +1,13 @@
-import json
 import requests
+from secretKeys import TWITCH_CLIENT_ID
 
-CLIENT_ID = None
-CLIENT_SECRET = None
+# taken from "Client Secret" at https://dev.twitch.tv/console/apps/
+TWITCH_CLIENT_SECRET = None
 
 # This code is derived from clicking this URL and then authenticating:
 # https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=CLIENT_ID_HERE&redirect_uri=http://localhost&scope=chat:read+chat:edit+channel:moderate+whispers:read+whispers:edit+channel_editor+channel:read:redemptions
-CODE_SECRET = None
+TWITCH_CODE_SECRET = None
 
-url = f'https://id.twitch.tv/oauth2/token?client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&code={CODE_SECRET}&grant_type=authorization_code&redirect_uri=http://localhost'
+url = f'https://id.twitch.tv/oauth2/token?client_id={TWITCH_CLIENT_ID}&client_secret={TWITCH_CLIENT_SECRET}&code={TWITCH_CODE_SECRET}&grant_type=authorization_code&redirect_uri=http://localhost'
 r = requests.post(url)
 print(r.content)
--- secretKeys.py
+++ secretKeys.py
@@ -1,11 +1,5 @@
-# acquired using fetchTokens.py
-TWITCH_ACCESS_TOKEN = None
-
 # taken from "Client ID" at https://dev.twitch.tv/console/apps/
 TWITCH_CLIENT_ID = None
 
 # generated from https://twitchapps.com/tmi/
 TWITCH_IRC_TOKEN = None
-
-# again, acquired using fetchTokens.py
-TWITCH_REFRESH_TOKEN = None
