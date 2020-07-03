ab63b9fb1eb9745a30cd652f53c6197da49e2fc0
Charles Madere <charlesrmadere@gmail.com>
2020-07-02 17:24:22 -0700

    fixed up first time running code a bit more

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from secretKeys import TWITCH_CLIENT_ID
 from secretKeys import TWITCH_IRC_TOKEN
 from users import USERS
 
+if TWITCH_CLIENT_ID == None or len(TWITCH_CLIENT_ID) == 0 or TWITCH_CLIENT_ID.isspace():
+    raise ValueError('secretKeys.TWITCH_CLIENT_ID can\'t be empty!')
+elif TWITCH_IRC_TOKEN == None or len(TWITCH_IRC_TOKEN) == 0 or TWITCH_IRC_TOKEN.isspace():
+    raise ValueError('secretKeys.TWITCH_IRC_TOKEN can\'t be empty!')
+elif USERS == None or len(USERS) == 0:
+    raise ValueError('USERS list can\'t be empty!')
+
 cynanBot = CynanBot(
     ircToken = TWITCH_IRC_TOKEN,
     clientId = TWITCH_CLIENT_ID,
--- users.py
+++ users.py
@@ -6,18 +6,5 @@ from user import User
 # inspect the incoming JSON data to determine that information.
 
 USERS = [
-    # User(
-    #     twitchHandle = "Imyt",
-    #     picOfTheDayFile = "/home/declan/potd.txt",
-    #     accessToken = "",
-    #     refreshToken = "",
-    #     rewardId = ""
-    # ),
-    User(
-        twitchHandle = "smCharles",
-        picOfTheDayFile = "/home/charles/potd.txt",
-        accessToken = "",
-        refreshToken = "",
-        rewardId = "fc83a5e8-4ff9-459b-ba98-64454964b5f8",
-    )
+    # place some users in here
 ]
