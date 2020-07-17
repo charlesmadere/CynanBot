2ba48e7a910042aaec477de38acdbd624e736746
Charles Madere <charlesrmadere@gmail.com>
2020-07-16 22:19:41 -0700

    some Python cleanup

--- user.py
+++ user.py
@@ -9,22 +9,27 @@ class User:
         rewardId: str,
         timeZone: str
     ):
+        if handle == None or len(handle) == 0 or handle.isspace():
+            raise ValueError(f'handle argument is malformed: \"{handle}\"')
+        elif picOfTheDayFile == None or len(picOfTheDayFile) == 0 or picOfTheDayFile.isspace():
+            raise ValueError(f'picOfTheDayFile argument is malformed: \"{picOfTheDayFile}\"')
+
         self.__handle = handle
         self.__picOfTheDayFile = picOfTheDayFile
         self.__rewardId = rewardId
         self.__timeZone = timeZone
 
     def fetchPicOfTheDay(self):
-        potdText = ""
-
         if not os.path.exists(self.__picOfTheDayFile):
             raise FileNotFoundError(f'POTD file not found: \"{self.__picOfTheDayFile}\"')
+
+        potdText = None
 
         with open(self.__picOfTheDayFile, 'r') as file:
             potdText = file.read().replace('\n', '').lstrip().rstrip()
 
-        if len(potdText) == 0 or potdText.isspace():
-            raise ValueError('POTD text is malformed!')
+        if potdText == None or len(potdText) == 0 or potdText.isspace():
+            raise ValueError(f'POTD text is malformed: \"{potdText}\"')
 
         potdParsed = urlparse(potdText)
         potdUrl = potdParsed.geturl()
