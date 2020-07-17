352107477822acc5aeab29aa857fcd26c9b8ddbc
Charles Madere <charlesrmadere@gmail.com>
2020-07-16 22:26:36 -0700

    fixed main.py to use new APIs

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from authHelper import AuthHelper
 from channelIdsRepository import ChannelIdsRepository
 from cynanBot import CynanBot
+from usersRepository import UsersRepository
+from userTokensRepository import UserTokensRepository
 
+authHelper = AuthHelper()
 channelIdsRepository = ChannelIdsRepository()
-authHelper = AuthHelper(channelIdsRepository = channelIdsRepository)
-cynanBot = CynanBot(authHelper = authHelper)
+usersRepository = UsersRepository()
+userTokensRepository = UserTokensRepository()
+
+cynanBot = CynanBot(
+    authHelper = authHelper,
+    channelIdsRepository = channelIdsRepository,
+    usersRepository = usersRepository,
+    userTokensRepository = userTokensRepository
+)
 
 print("Starting CynanBot...")
 cynanBot.run()
