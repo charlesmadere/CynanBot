ff6c3bb0f1de48887bf152ff0e4e96f44e5de025
Charles Madere <charlesrmadere@gmail.com>
2020-12-18 14:19:49 +0900

    some more python cleanup, and more use of Utils.isValidStr()

--- nonceRepository.py
+++ nonceRepository.py
@@ -1,20 +1,23 @@
+import utils
+
+
 class NonceRepository():
 
     def __init__(self):
         self.__cache = dict()
 
     def getNonce(self, key: str):
-        if key is None or len(key) == 0 or key.isspace():
+        if not utils.isValidStr(key):
             raise ValueError(f'key argument is malformed: \"{key}\"')
 
         key = key.lower()
         return self.__cache.get(key)
 
     def setNonce(self, key: str, nonce: str):
-        if key is None or len(key) == 0 or key.isspace():
+        if not utils.isValidStr(key):
             raise ValueError(f'key argument is malformed: \"{key}\"')
 
-        if nonce is None or len(nonce) == 0 or nonce.isspace():
+        if not utils.isValidStr(nonce):
             print(f'key \"{key}\" has an invalid nonce: \"{nonce}\"')
             self.__cache.pop(key, None)
             return
--- userIdsRepository.py
+++ userIdsRepository.py
@@ -2,6 +2,7 @@ import json
 
 import requests
 
+import utils
 from backingDatabase import BackingDatabase
 
 
@@ -9,8 +10,7 @@ class UserIdsRepository():
 
     def __init__(self, backingDatabase: BackingDatabase):
         if backingDatabase is None:
-            raise ValueError(
-                f'backingDatabase argument is malformed: \"{backingDatabase}\"')
+            raise ValueError(f'backingDatabase argument is malformed: \"{backingDatabase}\"')
 
         self.__backingDatabase = backingDatabase
 
@@ -31,12 +31,11 @@ class UserIdsRepository():
         clientId: str = None,
         accessToken: str = None
     ):
-        if userName is None or len(userName) == 0 or userName.isspace():
+        if not utils.isValidStr(userName):
             raise ValueError(f'userName argument is malformed: \"{userName}\"')
 
         cursor = self.__backingDatabase.getConnection().cursor()
-        cursor.execute(
-            'SELECT userId FROM userIds WHERE userName = ?', (userName, ))
+        cursor.execute('SELECT userId FROM userIds WHERE userName = ?', (userName, ))
         row = cursor.fetchone()
 
         userId = None
@@ -47,20 +46,16 @@ class UserIdsRepository():
 
         if userId is not None:
             if len(userId) == 0 or userId.isspace():
-                raise RuntimeError(
-                    f'Persisted userId for userName \"{userName}\" is malformed: \"{userId}\"')
+                raise RuntimeError(f'Persisted userId for userName \"{userName}\" is malformed: \"{userId}\"')
             else:
                 return userId
 
-        if clientId is None or len(clientId) == 0 or clientId.isspace():
-            print(
-                f'Can\'t lookup user ID for \"{userName}\", as clientId is malformed: \"{clientId}\"')
+        if not utils.isValidStr(clientId):
+            print(f'Can\'t lookup user ID for \"{userName}\", as clientId is malformed: \"{clientId}\"')
             raise ValueError(f'clientId argument is malformed: \"{clientId}\"')
-        elif accessToken is None or len(accessToken) == 0 or accessToken.isspace():
-            print(
-                f'Can\'t lookup user ID for \"{userName}\", as accessToken is malformed: \"{accessToken}\"')
-            raise ValueError(
-                f'accessToken argument is malformed: \"{accessToken}\"')
+        elif not utils.isValidStr(accessToken):
+            print(f'Can\'t lookup user ID for \"{userName}\", as accessToken is malformed: \"{accessToken}\"')
+            raise ValueError(f'accessToken argument is malformed: \"{accessToken}\"')
 
         print(f'Performing network call to fetch user ID for {userName}...')
 
@@ -77,43 +72,39 @@ class UserIdsRepository():
         jsonResponse = rawResponse.json()
 
         if 'error' in jsonResponse and len(jsonResponse['error']) >= 1:
-            raise RuntimeError(
-                f'Received an error when fetching user ID for {userName}: {jsonResponse}')
+            raise RuntimeError(f'Received an error when fetching user ID for {userName}: {jsonResponse}')
 
         userId = jsonResponse['data'][0]['id']
 
-        if userId is None or len(userId) == 0 or userId.isspace():
-            raise ValueError(
-                f'Unable to fetch user ID for {userName}: {jsonResponse}')
+        if not utils.isValidStr(userId):
+            raise ValueError(f'Unable to fetch user ID for {userName}: {jsonResponse}')
 
         self.setUser(userId=userId, userName=userName)
 
         return userId
 
     def fetchUserName(self, userId: str):
-        if userId is None or len(userId) == 0 or userId.isspace() or userId == '0':
+        if not utils.isValidStr(userId) or userId == '0':
             raise ValueError(f'userId argument is malformed: \"{userId}\"')
 
         cursor = self.__backingDatabase.getConnection().cursor()
-        cursor.execute(
-            'SELECT userName FROM userIds WHERE userId = ?', (userId, ))
+        cursor.execute('SELECT userName FROM userIds WHERE userId = ?', (userId, ))
         row = cursor.fetchone()
 
         if row is None:
             raise RuntimeError(f'No userName for userId \"{userId}\" found')
 
         userName = row[0]
-        if userName is None or len(userName) == 0 or userName.isspace():
-            raise RuntimeError(
-                f'userName for userId \"{userId}\" is malformed: \"{userName}\"')
+        if not utils.isValidStr(userName):
+            raise RuntimeError(f'userName for userId \"{userId}\" is malformed: \"{userName}\"')
 
         cursor.close()
         return userName
 
     def setUser(self, userId: str, userName: str):
-        if userId is None or len(userId) == 0 or userId.isspace() or userId == '0':
+        if not utils.isValidStr(userId) or userId == '0':
             raise ValueError(f'userId argument is malformed: \"{userId}\"')
-        elif userName is None or len(userName) == 0 or userName.isspace():
+        elif not utils.isValidStr(userName):
             raise ValueError(f'userName argument is malformed: \"{userName}\"')
 
         connection = self.__backingDatabase.getConnection()
