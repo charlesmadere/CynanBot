e668550df84a3c7cd3c4ddc7c58770e51246acda
Charles Madere <charlesrmadere@gmail.com>
2021-02-17 21:04:06 +0900

    another tiny log change

--- authHelper.py
+++ authHelper.py
@@ -169,7 +169,7 @@ class AuthHelper():
             jsonResponse = rawResponse.json()
 
             if jsonResponse.get('client_id') is None or len(jsonResponse['client_id']) == 0:
-                print(f'Refreshing access token for {handle}...')
+                print(f'Refreshing access token for \"{handle}\"...')
 
                 self.__refreshAccessToken(
                     handle = handle,
