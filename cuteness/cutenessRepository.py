48f64d2a7198db20f54afebc9dba6d899faaf227
Charles Madere <charlesrmadere@gmail.com>
2022-03-03 01:48:33 -0800

    added some code to help protect against gigantic cuteness incrementation values

--- cuteness/cutenessRepository.py
+++ cuteness/cutenessRepository.py
@@ -19,10 +19,18 @@ class CutenessRepository():
         userIdsRepository: UserIdsRepository,
         doubleCutenessTimeSeconds: int = 300,
         leaderboardSize: int = 10,
-        localLeaderboardSize: int = 5
+        localLeaderboardSize: int = 5,
+        maxValue: int = 2147483647,
+        minValue: int = -2147483648
     ):
         if backingDatabase is None:
             raise ValueError(f'backingDatabase argument is malformed: \"{backingDatabase}\"')
+        elif userIdsRepository is None:
+            raise ValueError(f'userIdsRepository argument is malformed: \"{userIdsRepository}\"')
+        elif not utils.isValidNum(doubleCutenessTimeSeconds):
+            raise ValueError(f'doubleCutenessTimeSeconds argument is malformed: \"{doubleCutenessTimeSeconds}\"')
+        elif doubleCutenessTimeSeconds < 15 or doubleCutenessTimeSeconds > 300:
+            raise ValueError(f'doubleCutenessTimeSeconds argument is out of bounds \"{doubleCutenessTimeSeconds}\"')
         elif not utils.isValidNum(leaderboardSize):
             raise ValueError(f'leaderboardSize argument is malformed: \"{leaderboardSize}\"')
         elif leaderboardSize < 3 or leaderboardSize > 10:
@@ -31,18 +39,22 @@ class CutenessRepository():
             raise ValueError(f'localLeaderboardSize argument is malformed: \"{localLeaderboardSize}\"')
         elif localLeaderboardSize < 1 or localLeaderboardSize > 5:
             raise ValueError(f'localLeaderboardSize argument is out of bounds: \"{localLeaderboardSize}\"')
-        elif userIdsRepository is None:
-            raise ValueError(f'userIdsRepository argument is malformed: \"{userIdsRepository}\"')
-        elif not utils.isValidNum(doubleCutenessTimeSeconds):
-            raise ValueError(f'doubleCutenessTimeSeconds argument is malformed: \"{doubleCutenessTimeSeconds}\"')
-        elif doubleCutenessTimeSeconds < 15 or doubleCutenessTimeSeconds > 300:
-            raise ValueError(f'doubleCutenessTimeSeconds argument is out of bounds \"{doubleCutenessTimeSeconds}\"')
+        elif not utils.isValidNum(maxValue):
+            raise ValueError(f'maxValue argument is malformed: \"{maxValue}\"')
+        elif maxValue <= 0:
+            raise ValueError(f'maxValue argument is out of bounds: \"{maxValue}\"')
+        elif not utils.isValidNum(minValue):
+            raise ValueError(f'minValue argument is malformed: \"{minValue}\"')
+        elif minValue >= 0:
+            raise ValueError(f'minValue argument is out of bounds: \"{minValue}\"')
 
         self.__backingDatabase: BackingDatabase = backingDatabase
-        self.__leaderboardSize: int = leaderboardSize
-        self.__localLeaderboardSize: int = localLeaderboardSize
         self.__userIdsRepository: UserIdsRepository = userIdsRepository
         self.__doubleCutenessTimeSeconds: int = doubleCutenessTimeSeconds
+        self.__leaderboardSize: int = leaderboardSize
+        self.__localLeaderboardSize: int = localLeaderboardSize
+        self.__maxValue: int = maxValue
+        self.__minValue: int = minValue
 
         self.__initDatabaseTable()
 
@@ -152,6 +164,10 @@ class CutenessRepository():
     ) -> CutenessResult:
         if not utils.isValidNum(incrementAmount):
             raise ValueError(f'incrementAmount argument is malformed: \"{incrementAmount}\"')
+        elif incrementAmount >= self.__maxValue:
+            raise ValueError(f'incrementAmount ({incrementAmount}) is >= maxValue ({self.__maxValue})')
+        elif incrementAmount <= self.__minValue:
+            raise ValueError(f'incrementAmount ({incrementAmount}) is <= minValue ({self.__minValue})')
         elif not utils.isValidStr(twitchChannel):
             raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
         elif not utils.isValidStr(userId):
