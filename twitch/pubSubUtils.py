7ba944d1c5ebaf264c1a5e5aed1dd3bfa25f4148
Charles Madere <charlesrmadere@gmail.com>
2022-04-04 23:59:40 -0700

    increased max pubsub connections per channel to 3

--- twitch/pubSubUtils.py
+++ twitch/pubSubUtils.py
@@ -54,7 +54,7 @@ class PubSubUtils():
         twitchTokensRepository: TwitchTokensRepository,
         userIdsRepository: UserIdsRepository,
         usersRepository: UsersRepository,
-        maxConnectionsPerTwitchChannel: int = 2
+        maxConnectionsPerTwitchChannel: int = 3
     ):
         if authRepository is None:
             raise ValueError(f'authRepository argument is malformed: \"{authRepository}\"')
