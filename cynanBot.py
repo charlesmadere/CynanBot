0ec7f28107a9ceaccbb969895c58d3f742c791ec
Charles Madere <charlesrmadere@gmail.com>
2022-05-14 14:50:37 -0700

    Revert "allow CynanBot's own messages into the ChatLogger"
    This reverts commit 228805c28c20b27c0c71caa7c61599e53542b90a.

--- cynanBot.py
+++ cynanBot.py
@@ -342,13 +342,6 @@ class CynanBot(commands.Bot):
             raise error
 
     async def event_message(self, message: Message):
-        twitchUser = await self.__usersRepository.getUserAsync(message.channel.name)
-
-        await self.__chatLogMessage.handleMessage(
-            twitchUser = twitchUser,
-            message = message
-        )
-
         if message.echo:
             return
 
@@ -358,6 +351,13 @@ class CynanBot(commands.Bot):
                     userId = str(message.author.id),
                     userName = message.author.name
                 )
+
+            twitchUser = await self.__usersRepository.getUserAsync(message.channel.name)
+
+            await self.__chatLogMessage.handleMessage(
+                twitchUser = twitchUser,
+                message = message
+            )
 
             if await self.__chatBandMessage.handleMessage(
                 twitchUser = twitchUser,
