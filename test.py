e4a97d2a64901db269f9ef9fe3cf18bf58eea774
Charles Madere <charlesrmadere@gmail.com>
2022-06-01 16:51:17 -0700

    progress on improved trivia answer checking

--- test.py
+++ test.py
@@ -7,33 +7,45 @@ from typing import IO, Any, Dict, List
 from aiofile import async_open
 
 import CynanBotCommon.utils as utils
+from CynanBotCommon.timber.timber import Timber
+from CynanBotCommon.trivia.questionAnswerTriviaQuestion import \
+    QuestionAnswerTriviaQuestion
+from CynanBotCommon.trivia.triviaAnswerChecker import TriviaAnswerChecker
 from CynanBotCommon.trivia.triviaAnswerCompiler import TriviaAnswerCompiler
+from CynanBotCommon.trivia.triviaDifficulty import TriviaDifficulty
+from CynanBotCommon.trivia.triviaSettingsRepository import \
+    TriviaSettingsRepository
+from CynanBotCommon.trivia.triviaSource import TriviaSource
+
+eventLoop = asyncio.get_event_loop()
+timber = Timber(eventLoop = eventLoop)
+triviaSettingsRepository = TriviaSettingsRepository()
 
 fileName = 'authRepository.json'
+
 triviaAnswerCompiler = TriviaAnswerCompiler()
-eventLoop = asyncio.get_event_loop()
+triviaAnswerChecker = TriviaAnswerChecker(
+    timber = timber,
+    triviaAnswerCompiler = triviaAnswerCompiler,
+    triviaSettingsRepository = triviaSettingsRepository
+)
+triviaQuestion = QuestionAnswerTriviaQuestion(
+    correctAnswers = [ 'boars' ],
+    cleanedCorrectAnswers = [ 'boars' ],
+    category = 'Animals',
+    question = 'What is another name for a pig?',
+    triviaId = 'abc123',
+    triviaDifficulty = TriviaDifficulty.UNKNOWN,
+    triviaSource = TriviaSource.J_SERVICE
+)
 
 async def doStuff():
-    jsonContents = None
+    result = await triviaAnswerChecker.checkAnswer(
+        answer = 'Boar',
+        triviaQuestion = triviaQuestion
+    )
 
-    async with async_open(fileName, 'r') as file:
-        data = await file.read()
-        jsonContents = json.loads(data)
-
-    print(jsonContents)
-
-    result = await triviaAnswerCompiler.compileTextAnswers([
-        '(Justice) Roberts'
-    ])
-    print(result)
-
-    result = await triviaAnswerCompiler.compileTextAnswer('<b>an bean bag chairs</b>\n\n')
-    print(f'\"{result}\"')
-
-    dictionary: List[Dict[str, object]] = {'id': 90431, 'answer': '<i>Brigadoon</i>', 'question': '"Almost Like Being In Love","I\'ll Go Home With Bonnie Jean"', 'value': 400, 'airdate': '2009-03-17T12:00:00.000Z', 'created_at': '2014-02-14T01:56:08.785Z', 'updated_at': '2014-02-14T01:56:08.785Z', 'category_id': 11951, 'game_id': None, 'invalid_count': None, 'category': {'id': 11951, 'title': 'shows by show tunes', 'created_at': '2014-02-14T01:56:08.399Z', 'updated_at': '2014-02-14T01:56:08.399Z', 'clues_count': 5}}
-    answer = '<i>Brigadoon</i>'
-    cleanedAnswer = utils.getStrFromDict(dictionary, 'answer', clean = True, removeCarrots = True)
-    print(f'{answer}, {cleanedAnswer}, {utils.cleanStr(answer, removeCarrots = True)}')
+    print(f'result: \"{result}\"')
 
     await asyncio.sleep(3)
     pass
