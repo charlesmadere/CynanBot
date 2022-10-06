8d832832c7a3871ab42420beb9d133e12a77fab9
Charles Madere <charlesmadere@users.noreply.github.com>
2022-10-05 23:02:54 -0700

    updated CynanBotCommon submodule

--- test.py
+++ test.py
@@ -2,8 +2,10 @@ import asyncio
 
 from CynanBotCommon.backingDatabase import BackingDatabase
 from CynanBotCommon.trivia.triviaEmoteGenerator import TriviaEmoteGenerator
+from CynanBotCommon.trivia.triviaAnswerCompiler import TriviaAnswerCompiler
 
 backingDatabase = BackingDatabase()
+triviaAnswerCompiler = TriviaAnswerCompiler()
 triviaEmoteGenerator = TriviaEmoteGenerator(backingDatabase)
 
 async def main():
@@ -13,53 +15,10 @@ async def main():
     emote = await triviaEmoteGenerator.getValidatedAndNormalizedEmote(original)
     print(f'{original}:{emote}')
 
-    print(await triviaEmoteGenerator.getNextEmoteFor('blah'))
+    answer = await triviaAnswerCompiler.compileTextAnswer('5')
+    print(f'answer="{answer}"')
 
-    connection = await backingDatabase.getConnection()
-    await connection.execute(
-        '''
-            ALTER TABLE triviaScores
-            RENAME COLUMN totalLosses TO triviaLosses
-        '''
-    )
-
-    await connection.execute(
-        '''
-            ALTER TABLE triviaScores
-            RENAME COLUMN totalWins TO triviaWins
-        '''
-    )
-
-    cursor = await connection.execute(
-        '''
-            SELECT streak, superTriviaWins, triviaLosses, triviaWins, twitchChannel, userId FROM triviaScores
-            ORDER BY twitchChannel ASC
-        '''
-    )
-
-    rows = await cursor.fetchall()
-
-    for row in rows:
-        streak: str = row[0]
-        superTriviaWins: int = row[1]
-        triviaLosses: int = row[2]
-        triviaWins: int = row[3]
-        twitchChannel: str = row[4]
-        userId: str = row[5]
-
-        newTriviaWins: int = triviaWins - superTriviaWins
-
-        await cursor.execute(
-            '''
-                UPDATE triviaScores
-                SET streak = ?, superTriviaWins = ?, triviaLosses = ?, triviaWins = ?
-                WHERE twitchChannel = ? AND userId = ?
-            ''',
-            ( streak, superTriviaWins, triviaLosses, newTriviaWins, twitchChannel, userId )
-        )
-
-    await connection.commit()
-    await cursor.close()
-    await connection.close()
+    answer = await triviaAnswerCompiler.compileTextAnswer('-5')
+    print(f'answer="{answer}"')
 
 asyncio.run(main())
