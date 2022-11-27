4f624d5a53ee12323c7d6b3040759cf644dcc7eb
Charles Madere <charlesmadere@users.noreply.github.com>
2022-11-26 19:29:51 -0800

    don't log duplicate reward IDs (for now at least)

--- cynanBot.py
+++ cynanBot.py
@@ -444,7 +444,6 @@ class CynanBot(commands.Bot, TriviaEventListener):
             self.__timber.log('CynanBot', f'Reward ID for {twitchUser.getHandle()}:{twitchUserIdStr} (redeemed by {userNameThatRedeemed}:{userIdThatRedeemed}): \"{rewardId}\"')
 
         if self.__channelPointsLruCache.contains(lruCacheId):
-            self.__timber.log('CynanBot', f'Encountered duplicate reward ID for {twitchUser.getHandle()}:{twitchUserIdStr} (redeemed by {userNameThatRedeemed}:{userIdThatRedeemed}): \"{event.id}\"')
             return
 
         self.__channelPointsLruCache.put(lruCacheId)
