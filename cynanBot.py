322847c8b8e830dcd48cf21009460c215c54820b
Charles Madere <charlesmadere@users.noreply.github.com>
2023-01-08 16:51:22 -0800

    fixed argument

--- cynanBot.py
+++ cynanBot.py
@@ -193,7 +193,7 @@ class CynanBot(commands.Bot, ModifyUserEventListener, TriviaEventListener):
         #######################################
 
         self.__addUserCommand: AbsCommand = AddUserCommand(generalSettingsRepository, modifyUserDataHelper, timber, twitchTokensRepository, twitchUtils, userIdsRepository, usersRepository)
-        self.__clearCachesCommand: AbsCommand = ClearCachesCommand(modifyUserDataHelper, authRepository, bannedWordsRepository, funtoonRepository, generalSettingsRepository, locationsRepository, timber, triviaSettingsRepository, twitchTokensRepository, twitchUtils, usersRepository, weatherRepository, wordOfTheDayRepository)
+        self.__clearCachesCommand: AbsCommand = ClearCachesCommand(authRepository, bannedWordsRepository, funtoonRepository, generalSettingsRepository, locationsRepository, modifyUserDataHelper, timber, triviaSettingsRepository, twitchTokensRepository, twitchUtils, usersRepository, weatherRepository, wordOfTheDayRepository)
         self.__commandsCommand: AbsCommand = CommandsCommand(generalSettingsRepository, timber, triviaUtils, twitchUtils, usersRepository)
         self.__confirmCommand: AbsCommand = ConfirmCommand(modifyUserDataHelper, generalSettingsRepository, timber, twitchUtils, usersRepository)
         self.__cynanSourceCommand: AbsCommand = CynanSourceCommand(timber, twitchUtils, usersRepository)
