916e9f4c0f39e92ec43c602d385ee1dd0f3bf52f
Charles Madere <charlesmadere@users.noreply.github.com>
2023-03-12 17:04:56 -0700

    updated CynanBotCommon submodule

--- pkmnTest.py
+++ pkmnTest.py
@@ -1,53 +1,36 @@
 import asyncio
-from asyncio import AbstractEventLoop
 
+from CynanBotCommon.backgroundTaskHelper import BackgroundTaskHelper
 from CynanBotCommon.network.networkClientProvider import NetworkClientProvider
 from CynanBotCommon.network.requestsClientProvider import \
     RequestsClientProvider
+from CynanBotCommon.pkmn.pokepediaDamageClass import PokepediaDamageClass
+from CynanBotCommon.pkmn.pokepediaElementType import PokepediaElementType
 from CynanBotCommon.pkmn.pokepediaGeneration import PokepediaGeneration
 from CynanBotCommon.pkmn.pokepediaRepository import PokepediaRepository
-from CynanBotCommon.storage.backingDatabase import BackingDatabase
-from CynanBotCommon.storage.backingSqliteDatabase import BackingSqliteDatabase
 from CynanBotCommon.timber.timber import Timber
 from CynanBotCommon.trivia.pkmnTriviaQuestionRepository import \
     PkmnTriviaQuestionRepository
-from CynanBotCommon.trivia.triviaEmoteGenerator import TriviaEmoteGenerator
 from CynanBotCommon.trivia.triviaIdGenerator import TriviaIdGenerator
 from CynanBotCommon.trivia.triviaSettingsRepository import \
     TriviaSettingsRepository
 
-eventLoop: AbstractEventLoop = asyncio.get_event_loop()
-
-timber = Timber(
-    eventLoop = eventLoop
-)
-
-networkClientProvider: NetworkClientProvider = RequestsClientProvider(
-    timber = timber
-)
+eventLoop = asyncio.get_event_loop()
+backgroundTaskHelper = BackgroundTaskHelper(eventLoop = eventLoop)
+timber = Timber(backgroundTaskHelper = backgroundTaskHelper)
+networkClientProvider: NetworkClientProvider = RequestsClientProvider(timber = timber)
 
 pokepediaRepository = PokepediaRepository(
     networkClientProvider = networkClientProvider,
     timber = timber
 )
 
-backingDatabase: BackingDatabase = BackingSqliteDatabase(
-    eventLoop = eventLoop
-)
-
-triviaEmoteGenerator = TriviaEmoteGenerator(
-    backingDatabase = backingDatabase,
-    timber = timber
-)
-
 triviaIdGenerator = TriviaIdGenerator()
-
 triviaSettingsRepository = TriviaSettingsRepository()
 
 pkmnTriviaQuestionRepository = PkmnTriviaQuestionRepository(
     pokepediaRepository = pokepediaRepository,
     timber = timber,
-    triviaEmoteGenerator = triviaEmoteGenerator,
     triviaIdGenerator = triviaIdGenerator,
     triviaSettingsRepository = triviaSettingsRepository
 )
@@ -55,11 +38,17 @@ pkmnTriviaQuestionRepository = PkmnTriviaQuestionRepository(
 async def main():
     pass
 
-    move = await pokepediaRepository.fetchRandomMove(PokepediaGeneration.GENERATION_3)
-    print(move)
+    # mon = await pokepediaRepository.searchPokemon('silvally')
+    # print(mon)
 
-    # question = await pkmnTriviaQuestionRepository.fetchTriviaQuestion(twitchChannel = 'smCharles')
-    # print(question)
+    # move = await pokepediaRepository.searchMoves('multi-attack')
+    # print(move)
+
+    blah = PokepediaDamageClass.getTypeBasedDamageClass(PokepediaElementType.BUG)
+    print(blah)
+
+    question = await pkmnTriviaQuestionRepository.fetchTriviaQuestion(twitchChannel = 'smCharles')
+    print(question)
 
     pass
 
