043b6699d2d33ba5aa9e82a7d8bfdf58bee88345
Charles Madere <charlesrmadere@gmail.com>
2023-08-20 21:32:50 +0900

    small str cleanup

--- commands.py
+++ commands.py
@@ -3536,7 +3536,7 @@ class TwitchInfoCommand(AbsCommand):
             return
 
         userInfoStr = await self.__toStr(userInfo)
-        await self.__twitchUtils.safeSend(ctx, f'ⓘ Twich info for — \"{userName}\"  {userInfoStr}')
+        await self.__twitchUtils.safeSend(ctx, f'ⓘ Twich info for {userName} — {userInfoStr}')
         self.__timber.log('TwitchInfoCommand', f'Handled !twitchinfo command for {ctx.getAuthorName()}:{ctx.getAuthorId()} in {user.getHandle()}')
 
     async def __toStr(self, userInfo: TwitchUserDetails) -> str:
