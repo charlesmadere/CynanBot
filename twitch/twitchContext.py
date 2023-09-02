e53d49144dd47b0a125a24b8590336e018d83968
Charles Madere <charlesrmadere@gmail.com>
2023-09-03 01:49:20 +0900

    small python cleanup

--- twitch/twitchContext.py
+++ twitch/twitchContext.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from typing import Optional
 
 from twitch.twitchAuthor import TwitchAuthor
@@ -7,32 +8,42 @@ from twitch.twitchMessageable import TwitchMessageable
 
 class TwitchContext(TwitchMessageable):
 
+    @abstractmethod
     def getAuthor(self) -> TwitchAuthor:
         pass
 
+    @abstractmethod
     def getAuthorDisplayName(self) -> str:
         pass
 
+    @abstractmethod
     def getAuthorId(self) -> str:
         pass
 
+    @abstractmethod
     def getAuthorName(self) -> str:
         pass
 
+    @abstractmethod
     def getMessageContent(self) -> Optional[str]:
         pass
 
+    @abstractmethod
     def getTwitchChannelName(self) -> str:
         pass
 
+    @abstractmethod
     def getTwitchConfigurationType(self) -> TwitchConfigurationType:
         pass
 
+    @abstractmethod
     def isAuthorMod(self) -> bool:
         pass
 
+    @abstractmethod
     def isAuthorVip(self) -> bool:
         pass
 
+    @abstractmethod
     async def send(self, message: str):
         pass
