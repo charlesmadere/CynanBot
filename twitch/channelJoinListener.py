a3d2a67a920f86c883818d861297c70d30659654
Charles Madere <charlesrmadere@gmail.com>
2023-09-30 15:27:14 +0900

    added abstract class info to `ChannelJoinListener`

--- twitch/channelJoinListener.py
+++ twitch/channelJoinListener.py
@@ -1,7 +1,10 @@
+from abc import ABC, abstractmethod
+
 from twitch.absChannelJoinEvent import AbsChannelJoinEvent
 
 
-class ChannelJoinListener():
+class ChannelJoinListener(ABC):
 
+    @abstractmethod
     async def onNewChannelJoinEvent(self, event: AbsChannelJoinEvent):
         pass
