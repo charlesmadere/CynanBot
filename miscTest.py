151d15bad37dbb2792eb1fae7d8c297e3c16c56c
Charles Madere <charlesrmadere@gmail.com>
2023-11-04 18:19:46 +0900

    updated CynanBotCommon submodule

--- miscTest.py
+++ miscTest.py
@@ -1,13 +1,108 @@
+import asyncio
 from typing import Any, Optional
 
-x = 'hello'
+from CynanBotCommon.contentScanner.bannedWordsRepository import \
+    BannedWordsRepository
+from CynanBotCommon.contentScanner.bannedWordsRepositoryInterface import \
+    BannedWordsRepositoryInterface
+from CynanBotCommon.contentScanner.contentScanner import ContentScanner
+from CynanBotCommon.contentScanner.contentScannerInterface import \
+    ContentScannerInterface
+from CynanBotCommon.emojiHelper.emojiHelper import EmojiHelper
+from CynanBotCommon.emojiHelper.emojiHelperInterface import \
+    EmojiHelperInterface
+from CynanBotCommon.emojiHelper.emojiRepository import EmojiRepository
+from CynanBotCommon.emojiHelper.emojiRepositoryInterface import \
+    EmojiRepositoryInterface
+from CynanBotCommon.storage.jsonFileReader import JsonFileReader
+from CynanBotCommon.storage.jsonStaticReader import JsonStaticReader
+from CynanBotCommon.storage.linesStaticReader import LinesStaticReader
+from CynanBotCommon.timber.timberInterface import TimberInterface
+from CynanBotCommon.timber.timberStub import TimberStub
+from CynanBotCommon.tts.decTalkCommandBuilder import DecTalkCommandBuilder
+from CynanBotCommon.tts.ttsCommandBuilderInterface import \
+    TtsCommandBuilderInterface
+from CynanBotCommon.tts.ttsSettingsRepository import TtsSettingsRepository
+from CynanBotCommon.tts.ttsSettingsRepositoryInterface import \
+    TtsSettingsRepositoryInterface
 
-def misc(query: str, *args: Optional[Any]):
-    print(args is None)
-    print(type(args))
-    print(len(args))
-    print(args)
-    print(tuple(args))
+timber: TimberInterface = TimberStub()
 
-misc(x)
-misc(x, 1, 3)
+bannedWordsRepository: BannedWordsRepositoryInterface = BannedWordsRepository(
+    bannedWordsLinesReader = LinesStaticReader(
+        lines = [ 'hydroxychloroquine' ]
+    ),
+    timber = timber
+)
+
+contentScanner: ContentScannerInterface = ContentScanner(
+    bannedWordsRepository = bannedWordsRepository,
+    timber = timber
+)
+
+emojiRepository: EmojiRepositoryInterface = EmojiRepository(
+    emojiJsonReader = JsonStaticReader(
+        jsonContents = {
+            'emojis': [
+                {
+                    'code': [
+                        "1F600"
+                    ],
+                    'emoji': '😀',
+                    'name': 'grinning face',
+                    'category': 'Smileys & Emotion',
+                    'subcategory': 'face-smiling',
+                    'support': {
+                        'apple': True,
+                        'google': True,
+                        'windows': True
+                    }
+                },
+                {
+                    'code': [
+                        "1F988"
+                    ],
+                    'emoji': '🦈',
+                    'name': 'shark',
+                    'category': 'Animals & Nature',
+                    'subcategory': 'animal-marine',
+                    'support': {
+                        'apple': True,
+                        'google': True,
+                        'windows': True
+                    }
+                }
+            ]
+        }
+    ),
+    timber = timber
+)
+
+emojiHelper: EmojiHelperInterface = EmojiHelper(
+    emojiRepository = emojiRepository
+)
+
+ttsSettingsRepository: TtsSettingsRepositoryInterface = TtsSettingsRepository(
+    settingsJsonReader = JsonStaticReader(
+        jsonContents = {
+            'isEnabled': True
+        }
+    )
+)
+
+decTalkCommandBuilder: TtsCommandBuilderInterface = DecTalkCommandBuilder(
+    contentScanner = contentScanner,
+    emojiHelper = emojiHelper,
+    timber = timber,
+    ttsSettingsRepository = ttsSettingsRepository
+)
+
+eventLoop = asyncio.get_event_loop()
+
+async def main():
+    pass
+    result = await decTalkCommandBuilder.buildAndCleanMessage('shark 🦈 shark 😀 🤔')
+    print(f'result=\"{result}\"')
+    pass
+
+eventLoop.run_until_complete(main())
