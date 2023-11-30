0d55ed78633c7510a565039b0ab5959fa9938705
Charles Madere <charlesrmadere@gmail.com>
2023-12-01 01:03:47 +0900

    fixed another import

--- src/CynanBot/recurringActions/recurringActionEventListener.py
+++ src/CynanBot/recurringActions/recurringActionEventListener.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-from recurringActions.recurringEvent import RecurringEvent
+from CynanBot.recurringActions.recurringEvent import RecurringEvent
 
 
 class RecurringActionEventListener(ABC):
