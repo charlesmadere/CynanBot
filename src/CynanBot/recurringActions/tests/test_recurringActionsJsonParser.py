d2b72403e82af448f35a20a7f7ae182adca3b594
Charles Madere <charlesrmadere@gmail.com>
2024-01-08 17:09:29 +0900

    added more tests for `RecurringActionsJsonParser`

--- src/CynanBot/recurringActions/tests/test_recurringActionsJsonParser.py
+++ src/CynanBot/recurringActions/tests/test_recurringActionsJsonParser.py
@@ -6,25 +6,87 @@ import pytest
 from CynanBot.language.languagesRepository import LanguagesRepository
 from CynanBot.language.languagesRepositoryInterface import \
     LanguagesRepositoryInterface
+from CynanBot.recurringActions.recurringAction import RecurringAction
 from CynanBot.recurringActions.recurringActionsJsonParser import \
     RecurringActionsJsonParser
 from CynanBot.recurringActions.recurringActionsJsonParserInterface import \
     RecurringActionsJsonParserInterface
 from CynanBot.recurringActions.superTriviaRecurringAction import \
     SuperTriviaRecurringAction
+from CynanBot.recurringActions.weatherRecurringAction import \
+    WeatherRecurringAction
+from CynanBot.recurringActions.wordOfTheDayRecurringAction import \
+    WordOfTheDayRecurringAction
+from CynanBot.timber.timberInterface import TimberInterface
+from CynanBot.timber.timberStub import TimberStub
 
 
 class TestRecurringActionsJsonParser():
 
     languagesRepository: LanguagesRepositoryInterface = LanguagesRepository()
 
+    timber: TimberInterface = TimberStub()
+
     parser: RecurringActionsJsonParserInterface = RecurringActionsJsonParser(
-        languagesRepository = languagesRepository
+        languagesRepository = languagesRepository,
+        timber = timber
     )
 
     @pytest.mark.asyncio
     async def test_toJson_withSuperTriviaAction(self):
         action = SuperTriviaRecurringAction(
+            enabled = True,
+            twitchChannel = 'smCharles'
+        )
+
+        jsonString = await self.parser.toJson(action)
+        assert isinstance(jsonString, str)
+        assert len(jsonString) != 0
+        assert not jsonString.isspace()
+
+        jsonObject = json.loads(jsonString)
+        assert isinstance(jsonObject, Dict)
+        assert len(jsonObject) == 0
+
+    @pytest.mark.asyncio
+    async def test_toJson_withWeatherRecurringAction_alertsOnlyFalse(self):
+        action = WeatherRecurringAction(
+            enabled = True,
+            twitchChannel = 'smCharles',
+            alertsOnly = False
+        )
+
+        jsonString = await self.parser.toJson(action)
+        assert isinstance(jsonString, str)
+        assert len(jsonString) != 0
+        assert not jsonString.isspace()
+
+        jsonObject = json.loads(jsonString)
+        assert isinstance(jsonObject, Dict)
+        assert len(jsonObject) == 1
+        assert jsonObject.get('alertsOnly', None) is action.isEnabled()
+
+    @pytest.mark.asyncio
+    async def test_toJson_withWeatherRecurringAction_alertsOnlyTrue(self):
+        action = WeatherRecurringAction(
+            enabled = True,
+            twitchChannel = 'smCharles',
+            alertsOnly = True
+        )
+
+        jsonString = await self.parser.toJson(action)
+        assert isinstance(jsonString, str)
+        assert len(jsonString) != 0
+        assert not jsonString.isspace()
+
+        jsonObject = json.loads(jsonString)
+        assert isinstance(jsonObject, Dict)
+        assert len(jsonObject) == 1
+        assert jsonObject.get('alertsOnly', None) is action.isEnabled()
+
+    @pytest.mark.asyncio
+    async def test_toJson_withWordOfTheDayRecurringAction_languageEntryNone(self):
+        action = WordOfTheDayRecurringAction(
             enabled = True,
             twitchChannel = 'smCharles'
         )
