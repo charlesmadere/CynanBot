5a9e6b390d4583b33c77a6d8e69f93a87e3ca588
Charles Madere <charlesrmadere@gmail.com>
2024-01-14 17:24:09 +0900

    added new `chatCommands` package

---
+++ src/CynanBot/chatCommands/addRecurringActionCommand.py
@@ -0,0 +1,41 @@
+from CynanBot.administratorProviderInterface import \
+    AdministratorProviderInterface
+from CynanBot.chatCommands.absChatCommand import AbsChatCommand
+from CynanBot.recurringActions.recurringActionsRepositoryInterface import \
+    RecurringActionsRepositoryInterface
+from CynanBot.timber.timberInterface import TimberInterface
+from CynanBot.twitch.configuration.twitchContext import TwitchContext
+from CynanBot.twitch.twitchUtilsInterface import TwitchUtilsInterface
+from CynanBot.users.usersRepositoryInterface import UsersRepositoryInterface
+
+
+class AddRecurringActionCommand(AbsChatCommand):
+
+    def __init__(
+        self,
+        administratorProvider: AdministratorProviderInterface,
+        recurringActionsRepository: RecurringActionsRepositoryInterface,
+        timber: TimberInterface,
+        twitchUtils: TwitchUtilsInterface,
+        usersRepository: UsersRepositoryInterface
+    ):
+        if not isinstance(administratorProvider, AdministratorProviderInterface):
+            raise ValueError(f'administratorProvider argument is malformed: \"{administratorProvider}\"')
+        elif not isinstance(recurringActionsRepository, RecurringActionsRepositoryInterface):
+            raise ValueError(f'recurringActionsRepository argument is malformed: \"{recurringActionsRepository}\"')
+        elif not isinstance(timber, TimberInterface):
+            raise ValueError(f'timber argument is malformed: \"{timber}\"')
+        elif not isinstance(twitchUtils, TwitchUtilsInterface):
+            raise ValueError(f'twitchUtils argument is malformed: \"{twitchUtils}\"')
+        elif not isinstance(usersRepository, UsersRepositoryInterface):
+            raise ValueError(f'usersRepository argument is malformed: \"{usersRepository}\"')
+
+        self.__administratorProvider: AdministratorProviderInterface = administratorProvider
+        self.__recurringActionsRepository: RecurringActionsRepositoryInterface = recurringActionsRepository
+        self.__timber: TimberInterface = timber
+        self.__twitchUtils: TwitchUtilsInterface = twitchUtils
+        self.__usersRepository: UsersRepositoryInterface = usersRepository
+
+    async def handleChatCommand(self, ctx: TwitchContext):
+        # TODO
+        pass
