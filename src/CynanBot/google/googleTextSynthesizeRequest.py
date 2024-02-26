0da12a3c69f7521c19d9daae227046dc9380e55c
Charles Madere <charlesrmadere@gmail.com>
2024-02-25 23:22:36 -0800

    filled out class body

--- src/CynanBot/google/googleTextSynthesizeRequest.py
+++ src/CynanBot/google/googleTextSynthesizeRequest.py
@@ -14,6 +14,33 @@ class GoogleTextSynthesizeRequest():
         voice: GoogleVoiceSelectionParams,
         audioConfig: GoogleVoiceAudioConfig
     ):
+        if not isinstance(input, GoogleTextSynthesisInput):
+            raise TypeError(f'input argument is malformed: \"{input}\"')
+        elif not isinstance(voice, GoogleVoiceSelectionParams):
+            raise TypeError(f'voice argument is malformed: \"{voice}\"')
+        elif not isinstance(audioConfig, GoogleVoiceAudioConfig):
+            raise TypeError(f'audioConfig argument is malformed: \"{audioConfig}\"')
+
         self.__input: GoogleTextSynthesisInput = input
         self.__voice: GoogleVoiceSelectionParams = voice
         self.__audioConfig: GoogleVoiceAudioConfig = audioConfig
+
+    def getAudioConfig(self) -> GoogleVoiceAudioConfig:
+        return self.__audioConfig
+
+    def getInput(self) -> GoogleTextSynthesisInput:
+        return self.__input
+
+    def getVoice(self) -> GoogleVoiceSelectionParams:
+        return self.__voice
+
+    def __repr__(self) -> str:
+        dictionary = self.toDictionary()
+        return str(dictionary)
+
+    def toDictionary(self) -> Dict[str, Any]:
+        return {
+            'audioConfig': self.__audioConfig,
+            'input': self.__input,
+            'voice': self.__voice
+        }
