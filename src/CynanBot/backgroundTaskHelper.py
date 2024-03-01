296f5f65bc3a19d1eb4993d9a31665ea9fcc2d1f
Charles Madere <charlesrmadere@gmail.com>
2024-02-29 21:04:58 -0800

    some small typing fixes

--- src/CynanBot/backgroundTaskHelper.py
+++ src/CynanBot/backgroundTaskHelper.py
@@ -1,4 +1,4 @@
-from asyncio import AbstractEventLoop
+from asyncio import AbstractEventLoop, Task
 from typing import Any, Coroutine, Set
 
 
@@ -6,14 +6,14 @@ class BackgroundTaskHelper():
 
     def __init__(self, eventLoop: AbstractEventLoop):
         if not isinstance(eventLoop, AbstractEventLoop):
-            raise ValueError(f'eventLoop argument is malformed: \"{eventLoop}\"')
+            raise TypeError(f'eventLoop argument is malformed: \"{eventLoop}\"')
 
         self.__eventLoop: AbstractEventLoop = eventLoop
-        self.__backgroundTasks: Set[Any] = set()
+        self.__backgroundTasks: Set[Task] = set()
 
     def createTask(self, coro: Coroutine):
-        if coro is None:
-            raise ValueError(f'coro argument is malformed: \"{coro}\"')
+        if not isinstance(coro, Coroutine):
+            raise TypeError(f'coro argument is malformed: \"{coro}\"')
 
         task = self.__eventLoop.create_task(coro)
         self.__backgroundTasks.add(task)
