bf91014c3fb27d7e17632ecb9d431652f088a24c
Charles Madere <charlesrmadere@gmail.com>
2024-03-08 00:53:49 +0900

    more boilerplate structure

--- src/CynanBot/trivia/triviaRepositories/glacialTriviaQuestionRepository.py
+++ src/CynanBot/trivia/triviaRepositories/glacialTriviaQuestionRepository.py
@@ -1,7 +1,14 @@
-from typing import Set
+from typing import Optional, Set
 
+import aiofiles
+import aiofiles.ospath
+import aiosqlite
+
+import CynanBot.misc.utils as utils
 from CynanBot.timber.timberInterface import TimberInterface
 from CynanBot.trivia.questions.absTriviaQuestion import AbsTriviaQuestion
+from CynanBot.trivia.questions.questionAnswerTriviaQuestion import \
+    QuestionAnswerTriviaQuestion
 from CynanBot.trivia.questions.triviaQuestionType import TriviaQuestionType
 from CynanBot.trivia.questions.triviaSource import TriviaSource
 from CynanBot.trivia.triviaFetchOptions import TriviaFetchOptions
@@ -21,21 +28,84 @@ class GlacialTriviaQuestionRepository(
     def __init__(
         self,
         timber: TimberInterface,
-        triviaSettingsRepository: TriviaSettingsRepositoryInterface
+        triviaSettingsRepository: TriviaSettingsRepositoryInterface,
+        triviaDatabaseFile: str = 'glacialTriviaQuestionsDatabase.sqlite'
     ):
         super().__init__(triviaSettingsRepository)
 
         if not isinstance(timber, TimberInterface):
             raise TypeError(f'timber argument is malformed: \"{timber}\"')
+        elif not utils.isValidStr(triviaDatabaseFile):
+            raise TypeError(f'triviaDatabaseFile argument is malformed: \"{triviaDatabaseFile}\"')
 
         self.__timber: TimberInterface = timber
+        self.__triviaDatabaseFile: str = triviaDatabaseFile
+
+        self.__hasQuestionSetAvailable: Optional[bool] = None
+
+    async def __fetchAnyTriviaQuestion(
+        self,
+        fetchOptions: TriviaFetchOptions
+    ) -> Optional[AbsTriviaQuestion]:
+        if not isinstance(fetchOptions, TriviaFetchOptions):
+            raise TypeError(f'fetchOptions argument is malformed: \"{fetchOptions}\"')
+
+        # TODO
+        raise RuntimeError('not implemented')
+
+    async def __fetchMultipleChoiceOrTrueFalseTriviaQuestion(
+        self,
+        fetchOptions: TriviaFetchOptions
+    ) -> Optional[AbsTriviaQuestion]:
+        if not isinstance(fetchOptions, TriviaFetchOptions):
+            raise TypeError(f'fetchOptions argument is malformed: \"{fetchOptions}\"')
+
+        # TODO
+        raise RuntimeError('not implemented')
+
+    async def __fetchQuestionAnswerTriviaQuestion(
+        self,
+        fetchOptions: TriviaFetchOptions
+    ) -> Optional[QuestionAnswerTriviaQuestion]:
+        if not isinstance(fetchOptions, TriviaFetchOptions):
+            raise TypeError(f'fetchOptions argument is malformed: \"{fetchOptions}\"')
+
+        connection = await aiosqlite.connect(self.__triviaDatabaseFile)
+        cursor = await connection.execute(
+            '''
+                SELECT answerA, answerB, answerC, answerD, answerE, answerF, correctAnswer, question, triviaDifficulty, triviaId, triviaType FROM glacialQuestions
+                ORDER BY RANDOM()
+                LIMIT 1
+            '''
+        )
+
+        # TODO
+
+        await cursor.close()
+        await connection.close()
+
+        raise RuntimeError('not implemented')
 
     async def fetchTriviaQuestion(self, fetchOptions: TriviaFetchOptions) -> AbsTriviaQuestion:
         if not isinstance(fetchOptions, TriviaFetchOptions):
             raise TypeError(f'fetchOptions argument is malformed: \"{fetchOptions}\"')
 
-        # TODO
-        raise RuntimeError('not yet implemented')
+        if not await aiofiles.ospath.exists(self.__triviaDatabaseFile):
+            raise FileNotFoundError(f'Glacial trivia database file not found: \"{self.__triviaDatabaseFile}\"')
+
+        question: Optional[AbsTriviaQuestion] = None
+
+        if fetchOptions.requireQuestionAnswerTriviaQuestion():
+            question = await self.__fetchQuestionAnswerTriviaQuestion(fetchOptions)
+        elif fetchOptions.areQuestionAnswerTriviaQuestionsEnabled():
+            question = await self.__fetchAnyTriviaQuestion(fetchOptions)
+        else:
+            question = await self.__fetchMultipleChoiceOrTrueFalseTriviaQuestion(fetchOptions)
+
+        if question is None:
+            raise RuntimeError('not yet implemented')
+
+        return question
 
     async def getSupportedTriviaTypes(self) -> Set[TriviaQuestionType]:
         return {
@@ -48,8 +118,13 @@ class GlacialTriviaQuestionRepository(
         return TriviaSource.GLACIAL
 
     async def hasQuestionSetAvailable(self) -> bool:
-        # TODO
-        return False
+        hasQuestionSetAvailable = self.__hasQuestionSetAvailable
+
+        if hasQuestionSetAvailable is None:
+            hasQuestionSetAvailable = await aiofiles.ospath.exists(self.__triviaDatabaseFile)
+            self.__hasQuestionSetAvailable = hasQuestionSetAvailable
+
+        return hasQuestionSetAvailable
 
     async def store(self, question: AbsTriviaQuestion) -> bool:
         if not isinstance(question, AbsTriviaQuestion):
