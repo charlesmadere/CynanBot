b428b0654a911e4670ae59b41e2cfcd9786ada27
Charles Madere <charlesrmadere@gmail.com>
2024-03-08 20:08:28 +0900

    variable name fix

--- src/CynanBot/trivia/triviaRepositories/glacialTriviaQuestionRepository.py
+++ src/CynanBot/trivia/triviaRepositories/glacialTriviaQuestionRepository.py
@@ -218,7 +218,7 @@ class GlacialTriviaQuestionRepository(
             )
         elif triviaType is TriviaQuestionType.TRUE_FALSE:
             return TrueFalseTriviaQuestion(
-                correctAnswers = answeers,
+                correctAnswers = answers,
                 category = category,
                 categoryId = categoryId,
                 question = question,
