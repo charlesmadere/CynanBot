af8866b8eb28cc98c54e48b12bf41e5e3e4303ac
Doug Hoskisson <beauxq@users.noreply.github.com>
2024-03-11 10:08:28 -0700

    function decorator for run-time type checking (#24)
    * function decorator for run-time type checking
    
    * test some abstract classes
    
    * most generics work now
    
    * don't consume Iterators
    
    * remove outdated docs

---
+++ src/CynanBot/misc/tests/test_type_check.py
@@ -0,0 +1,373 @@
+from abc import ABC, abstractmethod
+from collections import abc
+from enum import Enum
+import typing
+
+import pytest
+
+from CynanBot.misc.type_check import type_check
+
+
+def test_no_params() -> None:
+    """ don't know why you'd do this, but it shouldn't break """
+
+    @type_check
+    def f() -> None:
+        pass
+
+    f()
+
+
+def test_bad_name_kwarg() -> None:
+
+    @type_check
+    def f() -> None:
+        pass
+
+    with pytest.raises(TypeError):
+        f(x=3)  # type: ignore
+
+
+def test_too_many_arg() -> None:
+
+    @type_check
+    def f() -> None:
+        pass
+
+    with pytest.raises(TypeError):
+        f("3")  # type: ignore
+
+
+def test_int_for_float() -> None:
+    """ int should be accepted when param is float """
+
+    @type_check
+    def f(x: float) -> None:
+        pass
+
+    f(42)
+    f(2.2)
+
+
+def test_float_for_int() -> None:
+    """ float should not be accepted where int is required """
+
+    @type_check
+    def f(x: int) -> None:
+        pass
+
+    f(7)
+
+    with pytest.raises(TypeError):
+        f(9.9)  # type: ignore
+
+
+def test_int_for_opt_float() -> None:
+
+    @type_check
+    def f(x: float | None) -> None:
+        pass
+
+    f(42)
+    f(3.14)
+    f(None)
+
+
+def test_float_for_opt_int() -> None:
+    """ float should not be accepted where int | None is required """
+
+    @type_check
+    def f(x: int | None) -> None:
+        pass
+
+    with pytest.raises(TypeError):
+        f(9.9)  # type: ignore
+
+
+def test_none_union() -> None:
+
+    @type_check
+    def f(x: str | None) -> str:
+        return f"{x}"
+
+    f("hello")
+    f(None)
+
+    with pytest.raises(TypeError):
+        f(1.1)  # type: ignore
+
+
+def test_none_alone() -> None:
+    """ don't know why you would want to do this, but shouldn't break """
+
+    @type_check
+    def f(x: None) -> int:
+        return 3
+
+    f(None)
+
+    with pytest.raises(TypeError):
+        f([])  # type: ignore
+
+
+def test_typing_generic() -> None:
+
+    @type_check
+    def f(x: typing.Iterable[str]) -> str | None:
+        return "abc"
+
+    f("abc")  # This is still a headache.
+    f(frozenset({"a", "b", "c"}))
+    f({"x": 3, "y": 4, "z": None})
+    f(list())
+
+    with pytest.raises(TypeError):
+        # checks every item to make sure it's str
+        f(["a", "b", b"c"])  # type: ignore
+
+    with pytest.raises(TypeError):
+        # iterable but not giving str
+        f(b"abc")  # type: ignore
+
+
+def test_abc_generic() -> None:
+
+    @type_check
+    def f(x: abc.Sequence[float]) -> str | None:
+        return "abc"
+
+    f([1.0, 2.0, 3.0])
+    f(())
+    f((3,))
+    f(b"abc")
+
+    with pytest.raises(TypeError):
+        f("abc")  # type: ignore
+
+    with pytest.raises(TypeError):
+        f(8.3)  # type: ignore
+
+
+def test_builtin_generic() -> None:
+
+    @type_check
+    def f(x: dict[str, dict[bytes, int | None]]) -> str | None:
+        return "abc"
+
+    f({})
+    f({
+        "a": {
+            b"x": 5,
+            b"y": 8,
+            b"z": None
+        },
+        "b": {}
+    })
+
+    with pytest.raises(TypeError):
+        f(3)  # type: ignore
+
+    with pytest.raises(TypeError):
+        f({"a"})  # type: ignore
+
+    with pytest.raises(TypeError):
+        f({
+            "a": {
+                b"x": 5,
+                b"y": 8.2,  # type: ignore
+                b"z": None
+            },
+            "b": {}
+        })
+
+    with pytest.raises(TypeError):
+        f({
+            "a": {
+                b"x": 5,
+                b"y": 8,
+                "z": None  # type: ignore
+            },
+            "b": {}
+        })
+
+
+def test_non_iterable_generic() -> None:
+    """ only implementations of `Iterable` and `Mapping` work """
+
+    with pytest.raises(TypeError):
+        @type_check
+        def f(y: abc.Container[str]) -> typing.Literal["f"]:
+            return "f"
+
+
+def test_literal() -> None:
+    """ This could probably be implemented. But it doesn't work for now. """
+
+    with pytest.raises(TypeError):
+        @type_check
+        def f(y: typing.Literal["g"] | None = None) -> int | None:
+            return 3
+
+
+def test_custom_class() -> None:
+
+    class C:
+        pass
+
+    @type_check
+    def f(x: C) -> list[str]:
+        return []
+
+    f(C())
+
+    with pytest.raises(TypeError):
+        f(4)  # type: ignore
+
+
+def test_enum() -> None:
+
+    class E(Enum):
+        e = 1
+
+    @type_check
+    def f(x: E) -> abc.Mapping[int, str | None]:
+        return {}
+
+    f(E.e)
+
+    with pytest.raises(TypeError):
+        f(1)  # type: ignore
+
+
+def test_str_annotations() -> None:
+    """ str annotations not supported """
+
+    with pytest.raises(TypeError):
+        @type_check
+        def f(x: float, y: "str", z: object) -> object:
+            return x
+
+
+def test_second_arg() -> None:
+
+    @type_check
+    def f(x: int, y: float, z: str) -> int | None:
+        return int(y)
+
+    f(5, 5, "5")
+
+    with pytest.raises(TypeError):
+        f(5, 5j, "5")  # type: ignore
+
+
+def test_method() -> None:
+
+    class C:
+
+        @type_check
+        def f(self, x: object, y: str, z: bool | None) -> bool:
+            return False
+
+    c = C()
+
+    c.f(c, "c", True)
+    c.f(None, f"{c}", None)
+
+    with pytest.raises(TypeError):
+        c.f(None, c, False)  # type: ignore
+
+
+def test_with_defaults() -> None:
+
+    @type_check
+    def f(x: str, y: object, z: int | None = None) -> dict[bool, bool]:
+        return {}
+
+    f("a", b"b")
+    f("a", b"b", 3)
+
+    with pytest.raises(TypeError):
+        f(b"b", "a")  # type: ignore
+
+    with pytest.raises(TypeError):
+        f("a", 2.2, "c")  # type: ignore
+
+    with pytest.raises(TypeError):
+        f(1, b"b", 3)  # type: ignore
+
+
+def test_with_mixed_arg_kwarg() -> None:
+
+    @type_check
+    def f(x: bool | None, *, y: str, z: bool = True) -> None:
+        pass
+
+    f(True, y="f")
+    f(x=False, y="True", z=True)
+
+    with pytest.raises(TypeError):
+        f(True, y="f", z=8)  # type: ignore
+
+    with pytest.raises(TypeError):
+        f(x=b"False", y="8")  # type: ignore
+
+
+def test_interface() -> None:
+    class B(ABC):
+        pass
+
+    class C(B):
+        pass
+
+    class D:
+        pass
+
+    @type_check
+    def f(b: B) -> frozenset[str]:
+        return frozenset()
+
+    c = C()
+    f(c)
+
+    d = D()
+    with pytest.raises(TypeError):
+        f(d)  # type: ignore
+
+
+def test_interface_method() -> None:
+
+    class B(ABC):
+        @abstractmethod
+        def f(self, x: bytes | None) -> int:
+            """ interface method definition """
+
+    class C(B):
+        @type_check
+        def f(self, x: bytes | None) -> int:
+            return 0
+
+    b: B = C()
+    b.f(b"f")
+
+    with pytest.raises(TypeError):
+        b.f("f")  # type: ignore
+
+
+def test_consumable_iterable() -> None:
+    """ make sure the type checker doesn't consume the consumable iterable """
+
+    def iterator() -> abc.Iterator[int]:
+        yield 5
+
+    @type_check
+    def f(x: abc.Iterable[int]) -> bytearray:
+        return bytearray()
+
+    it = iterator()
+
+    f(it)
+
+    count = 0
+    for _ in it:
+        count += 1
+    assert count == 1, f"{count=}"
---
+++ src/CynanBot/misc/type_check.py
@@ -0,0 +1,160 @@
+from collections.abc import Iterable, Iterator, Mapping
+import logging
+from types import GenericAlias, UnionType
+from typing import Callable, NoReturn, ParamSpec, TypeGuard, TypeVar
+import typing
+from inspect import signature
+
+_Checkable = type | UnionType | GenericAlias | None
+
+
+def _is_generic_alias(obj: object) -> TypeGuard[GenericAlias]:
+    """
+    `typing._GenericAlias` is undocumented, so it could break with Python update.
+
+    It doesn't have everything that `types.GenericAlias` has, but it has what we need.
+    """
+    # TypeGuard could change to PEP 742 when available
+    return isinstance(obj, GenericAlias) or isinstance(obj, getattr(typing, "_GenericAlias"))
+
+
+def _check_type(name: str, expected_type: _Checkable, value: object) -> None:
+    """ raises TypeError if `value` is not `expected_type` """
+
+    def error() -> NoReturn:
+        raise TypeError(f"MALFORMED!! teh thingz!!!!1 oh, noes! guwuDesk {name=} {value=} {expected_type=}")
+
+    if expected_type is None:
+        if value is not None:
+            error()
+    elif isinstance(expected_type, type):
+        if expected_type is float:
+            if not isinstance(value, (float, int)):
+                error()
+        else:
+            if not isinstance(value, expected_type):
+                error()
+    elif _is_generic_alias(expected_type):
+        if not isinstance(value, expected_type.__origin__):
+            error()
+        type_vars = expected_type.__args__
+        if len(type_vars) == 1:
+            assert isinstance(value, Iterable), f"not iterable {name=} {value=} {expected_type=}"
+            # if this is an Iterator, we don't want to check the values inside, because it can consume them
+            if not isinstance(value, Iterator):
+                for each_value in value:
+                    _check_type(f"item in {name}", type_vars[0], each_value)
+        else:
+            assert len(type_vars) == 2, f"{name=} {value=} {expected_type=}"
+            assert isinstance(value, Mapping), f"not mapping {name=} {value=} {expected_type=}"
+            for key in value:
+                each_value = value[key]
+                _check_type(f"key in {name}", type_vars[0], key)
+                _check_type(f"value in {name}", type_vars[1], each_value)
+    else:
+        # assert_type(expected_type, UnionType)  # need pep 742 for _is_generic_alias
+        assert isinstance(expected_type, UnionType)
+        found_valid = False
+        for u_type in expected_type.__args__:
+            got_type_error = False
+            try:
+                _check_type(name, u_type, value)
+            except TypeError:
+                got_type_error = True
+            if not got_type_error:
+                found_valid = True
+                break
+        if not found_valid:
+            error()
+
+
+_names_that_dont_need_annotations = frozenset([
+    "return", "self"
+])
+
+
+def _is_checkable(annotations: dict[str, object]) -> TypeGuard[dict[str, _Checkable]]:
+    """
+    raise TypeError if there's some annotation that can't be checked
+
+    return True otherwise (never returns False)
+    """
+
+    def individual(name: str, ann: object) -> TypeGuard[_Checkable]:
+        if ann is None:
+            return True
+        if isinstance(ann, str):
+            # I've heard some libraries use eval for this.
+            # It could get tricky.
+            # Let's see how often it comes up.
+            logging.warning(f"str annotation found {ann=}")
+        if isinstance(ann, type):
+            # recursion base case
+            return True
+        if isinstance(ann, UnionType) and all(individual(name, a) for a in ann.__args__):
+            return True
+        if _is_generic_alias(ann):
+            if len(ann.__args__) == 1:  # 1 type var
+                if (
+                    isinstance(ann.__origin__, type) and
+                    issubclass(ann.__origin__, Iterable) and
+                    individual(f"type var of {name}", ann.__args__[0])
+                ):
+                    return True
+            elif len(ann.__args__) == 2:  # 2 type vars
+                if (
+                    isinstance(ann.__origin__, type) and
+                    issubclass(ann.__origin__, Mapping) and
+                    individual(f"key type of {name}", ann.__args__[0]) and
+                    individual(f"value type of {name}", ann.__args__[1])
+                ):
+                    return True
+            # fall through to raise
+        raise TypeError(f"uncheckable annotation {name=} {ann=} {type(ann)=}")
+
+    return all(
+        individual(name, ann)
+        for name, ann in annotations.items()
+        if name not in _names_that_dont_need_annotations
+    )
+
+
+_P = ParamSpec('_P')
+_RT = TypeVar("_RT")
+
+
+def type_check(func: Callable[_P, _RT]) -> Callable[_P, _RT]:
+    """ apply run-time type checking to the parameters of this function """
+
+    annotations = func.__annotations__
+    assert _is_checkable(annotations)
+    sig = signature(func)
+    params = sig.parameters
+
+    # make sure all parameters have a type annotation
+    for param_name in params:
+        if param_name not in annotations and param_name not in _names_that_dont_need_annotations:
+            raise TypeError(f"{param_name=} missing type annotation")
+
+    def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _RT:
+
+        # positional arguments
+        for param_name, param_value in zip(params, args):
+            if param_name not in annotations:
+                assert param_name == "self", f"samusConf {param_name=}"
+                continue
+            param_type = annotations[param_name]
+            _check_type(param_name, param_type, param_value)
+
+        # keyword arguments
+        for param_name, param_value in kwargs.items():
+            if param_name not in params:
+                # let it go through - will get normal "no argument named" error
+                continue
+            param_type = annotations[param_name]
+            _check_type(param_name, param_type, param_value)
+
+        result = func(*args, **kwargs)
+        return result
+
+    return wrapper
