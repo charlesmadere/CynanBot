c54d2bf1604f29d5ac0df5e1cf69560e5706d1b7
Charles Madere <charlesrmadere@gmail.com>
2024-03-23 17:52:24 +0900

    added more fields/arguments for the new `GoogleJwtBuilder` class

--- src/CynanBot/google/googleJwtBuilder.py
+++ src/CynanBot/google/googleJwtBuilder.py
@@ -1,3 +1,6 @@
+import CynanBot.misc.utils as utils
+from CynanBot.google.googleCloudProjectCredentialsProviderInterface import \
+    GoogleCloudProjectCredentialsProviderInterface
 from CynanBot.google.googleJsonMapperInterface import GoogleJsonMapperInterface
 from CynanBot.google.googleJwtBuilderInterface import GoogleJwtBuilderInterface
 from CynanBot.google.googleScope import GoogleScope
@@ -7,21 +10,29 @@ class GoogleJwtBuilder(GoogleJwtBuilderInterface):
 
     def __init__(
         self,
+        googleCloudCredentialsProvider: GoogleCloudProjectCredentialsProviderInterface,
         googleJsonMapper: GoogleJsonMapperInterface,
         googleScopes: set[GoogleScope] = {
             GoogleScope.CLOUD_TEXT_TO_SPEECH,
             GoogleScope.CLOUD_TRANSLATION
-        }
+        },
+        googleAssertionTarget: str = 'https://oauth2.googleapis.com/token'
     ):
-        if not isinstance(googleJsonMapper, GoogleJsonMapperInterface):
+        if not isinstance(googleCloudCredentialsProvider, GoogleCloudProjectCredentialsProviderInterface):
+            raise TypeError(f'(googleCloudCredentialsProvider argument is malformed: \"{googleCloudCredentialsProvider}\"')
+        elif not isinstance(googleJsonMapper, GoogleJsonMapperInterface):
             raise TypeError(f'googleJsonMapper argument is malformed: \"{googleJsonMapper}\"')
         elif not isinstance(googleScopes, set):
             raise TypeError(f'googleScopes argument is malformed: \"{googleScopes}\"')
         elif len(googleScopes) == 0:
             raise ValueError(f'googleScopes argument is empty: \"{googleScopes}\"')
+        elif not utils.isValidUrl(googleAssertionTarget):
+            raise TypeError(f'googleAssertionTarget argument is malformed: \"{googleAssertionTarget}\"')
 
+        self.__googleCloudCredentialsProvider: GoogleCloudProjectCredentialsProviderInterface = googleCloudCredentialsProvider
         self.__googleJsonMapper: GoogleJsonMapperInterface = googleJsonMapper
         self.__googleScopes: set[GoogleScope] = googleScopes
+        self.__googleAssertionTarget: str = googleAssertionTarget
 
         self.__scopesString: str | None = None
 
