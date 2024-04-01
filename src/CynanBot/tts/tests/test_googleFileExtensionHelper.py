b03e3ad3d558f494a7d5a5de0b5ba734972c0d6b
Charles Madere <charlesrmadere@gmail.com>
2024-04-01 20:09:39 +0900

    added more tests

--- src/CynanBot/tts/tests/test_googleFileExtensionHelper.py
+++ src/CynanBot/tts/tests/test_googleFileExtensionHelper.py
@@ -12,11 +12,47 @@ class TestGoogleFileExtensionHelper():
     fileExtensionHelper: GoogleFileExtensionHelperInterface = GoogleFileExtensionHelper()
 
     @pytest.mark.asyncio
-    async def text_getFileExtension_withMp3(self):
+    async def test_getFileExtension_withAlaw(self):
+        result: str | None = None
+
+        with pytest.raises(RuntimeError):
+            result = await self.fileExtensionHelper.getFileExtension(GoogleVoiceAudioEncoding.ALAW)
+
+        assert result is None
+
+    @pytest.mark.asyncio
+    async def test_getFileExtension_withLinear16(self):
+        result: str | None = None
+
+        with pytest.raises(RuntimeError):
+            result = await self.fileExtensionHelper.getFileExtension(GoogleVoiceAudioEncoding.LINEAR_16)
+
+        assert result is None
+
+    @pytest.mark.asyncio
+    async def test_getFileExtension_withMp3(self):
         result = await self.fileExtensionHelper.getFileExtension(GoogleVoiceAudioEncoding.MP3)
         assert result == 'mp3'
 
     @pytest.mark.asyncio
-    async def text_getFileExtension_withOggOpus(self):
+    async def test_getFileExtension_withMulaw(self):
+        result: str | None = None
+
+        with pytest.raises(RuntimeError):
+            result = await self.fileExtensionHelper.getFileExtension(GoogleVoiceAudioEncoding.MULAW)
+
+        assert result is None
+
+    @pytest.mark.asyncio
+    async def test_getFileExtension_withOggOpus(self):
         result = await self.fileExtensionHelper.getFileExtension(GoogleVoiceAudioEncoding.OGG_OPUS)
         assert result == 'ogg'
+
+    @pytest.mark.asyncio
+    async def test_getFileExtension_withUnspecified(self):
+        result: str | None = None
+
+        with pytest.raises(RuntimeError):
+            result = await self.fileExtensionHelper.getFileExtension(GoogleVoiceAudioEncoding.UNSPECIFIED)
+
+        assert result is None
