e013b90cfa4940805752ee323ed2b0806dde8bdc
Charles Madere <charlesrmadere@gmail.com>
2024-04-20 18:21:00 +0900

    use some data classes

--- src/CynanBot/aniv/mostRecentAnivMessage.py
+++ src/CynanBot/aniv/mostRecentAnivMessage.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 
 
-@dataclass(frozen=True)
+@dataclass(frozen = True)
 class MostRecentAnivMessage():
     dateTime: datetime
     message: str | None
