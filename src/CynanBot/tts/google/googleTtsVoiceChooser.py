cd0809092c6111af852d9dd3b36f76c92e2a0ca1
Charles Madere <charlesrmadere@gmail.com>
2024-04-29 03:04:27 +0900

    a bunch of Google TTS voice cleanup and fixes. for example, crackling US voice was removed

--- src/CynanBot/tts/google/googleTtsVoiceChooser.py
+++ src/CynanBot/tts/google/googleTtsVoiceChooser.py
@@ -13,10 +13,10 @@ class GoogleTtsVoiceChooser(GoogleTtsVoiceChooserInterface):
         languageCodeToNames: dict[str, set[str]] = {
             'en-AU': { 'en-AU-Neural2-A', 'en-AU-Neural2-B', 'en-AU-Neural2-C', 'en-AU-Neural2-D', 'en-AU-Neural2-A', 'en-AU-Neural2-B', 'en-AU-Neural2-C', 'en-AU-Neural2-D' },
             'en-GB': { 'en-GB-Neural2-A', 'en-GB-Neural2-B', 'en-GB-Neural2-C', 'en-GB-Neural2-D', 'en-GB-Neural2-F', 'en-GB-Wavenet-A', 'en-GB-Wavenet-B', 'en-GB-Wavenet-C', 'en-GB-Wavenet-D', 'en-GB-Wavenet-F' },
-            'en-US': { 'en-US-Casual-K', 'en-US-Journey-D', 'en-US-Journey-F' },
-            'fr-CA': { 'fr-CA-Wavenet-A', 'fr-CA-Wavenet-B', 'fr-CA-Wavenet-C', 'fr-CA-Wavenet-D' },
+            'en-US': { 'en-US-Casual-K', 'en-US-Journey-F' },
+            'fr-CA': { 'fr-CA-Neural2-A', 'fr-CA-Neural2-B', 'fr-CA-Neural2-C', 'fr-CA-Neural2-D' },
             'ja-JP': { 'ja-JP-Neural2-B', 'ja-JP-Neural2-C', 'ja-JP-Neural2-D' },
-            'sv-SE': { 'sv-SE-Wavenet-A', 'sv-SE-Wavenet-B', 'sv-SE-Wavenet-C', 'sv-SE-Wavenet-D', 'sv-SE-Wavenet-E' }
+            'sv-SE': { 'sv-SE-Standard-A', 'sv-SE-Standard-B', 'sv-SE-Standard-C', 'sv-SE-Standard-D', 'sv-SE-Standard-E' }
         }
     ):
         if not isinstance(languageCodeToNames, dict):
