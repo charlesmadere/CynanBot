bc01edc41c339363532c5f9ff0b4b9239638c43b
Charles Madere <charlesrmadere@gmail.com>
2024-05-25 20:52:04 +0900

    `TwitchOutcomePredictor` is now a frozen Python dataclass

--- src/CynanBot/twitch/api/twitchOutcomePredictor.py
+++ src/CynanBot/twitch/api/twitchOutcomePredictor.py
@@ -1,63 +1,10 @@
-from typing import Any, Dict, Optional
-
-import CynanBot.misc.utils as utils
+from dataclasses import dataclass
 
 
+@dataclass(frozen = True)
 class TwitchOutcomePredictor():
-
-    def __init__(
-        self,
-        channelPointsUsed: int,
-        channelPointsWon: Optional[int],
-        userId: str,
-        userLogin: str,
-        userName: str
-    ):
-        if not utils.isValidInt(channelPointsUsed):
-            raise TypeError(f'channelPointsUsed argument is malformed: \"{channelPointsUsed}\"')
-        elif channelPointsUsed < 0 or channelPointsUsed > utils.getLongMaxSafeSize():
-            raise ValueError(f'channelPointsUsed argument is out of bounds: {channelPointsUsed}')
-        elif channelPointsWon is not None and not utils.isValidInt(channelPointsWon):
-            raise TypeError(f'channelPointsWon argument is malformed: \"{channelPointsWon}\"')
-        elif channelPointsWon is not None and (channelPointsWon < 0 or channelPointsWon > utils.getLongMaxSafeSize()):
-            raise ValueError(f'channelPointsWon argument is out of bounds: {channelPointsWon}')
-        elif not utils.isValidStr(userId):
-            raise TypeError(f'userId argument is malformed: \"{userId}\"')
-        elif not utils.isValidStr(userLogin):
-            raise TypeError(f'userLogin argument is malformed: \"{userLogin}\"')
-        elif not utils.isValidStr(userName):
-            raise TypeError(f'userName argument is malformed: \"{userName}\"')
-
-        self.__channelPointsUsed: int = channelPointsUsed
-        self.__channelPointsWon: Optional[int] = channelPointsWon
-        self.__userId: str = userId
-        self.__userLogin: str = userLogin
-        self.__userName: str = userName
-
-    def getChannelPointsUsed(self) -> int:
-        return self.__channelPointsUsed
-
-    def getChannelPointsWon(self) -> Optional[int]:
-        return self.__channelPointsWon
-
-    def getUserId(self) -> str:
-        return self.__userId
-
-    def getUserLogin(self) -> str:
-        return self.__userLogin
-
-    def getUserName(self) -> str:
-        return self.__userName
-
-    def __repr__(self) -> str:
-        dictionary = self.toDictionary()
-        return str(dictionary)
-
-    def toDictionary(self) -> Dict[str, Any]:
-        return {
-            'channelPointsUsed': self.__channelPointsUsed,
-            'channelPointsWon': self.__channelPointsWon,
-            'userId': self.__userId,
-            'userLogin': self.__userLogin,
-            'userName': self.__userName
-        }
+    channelPointsUsed: int
+    channelPointsWon: int | None
+    userId: str
+    userLogin: str
+    userName: str
