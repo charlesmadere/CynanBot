1385f7cf1ff75f7b6ade7f0dd8660d1df253729d
Charles Madere <charlesrmadere@gmail.com>
2024-05-25 18:35:13 +0900

    switch another series of `if` statements to `match` statement

--- src/CynanBot/soundPlayerManager/soundAlertJsonMapper.py
+++ src/CynanBot/soundPlayerManager/soundAlertJsonMapper.py
@@ -23,44 +23,26 @@ class SoundAlertJsonMapper(SoundAlertJsonMapperInterface):
 
         jsonString = jsonString.lower()
 
-        if jsonString == 'cheer':
-            return SoundAlert.CHEER
-        elif jsonString == 'point_redemption_01':
-            return SoundAlert.POINT_REDEMPTION_01
-        elif jsonString == 'point_redemption_02':
-            return SoundAlert.POINT_REDEMPTION_02
-        elif jsonString == 'point_redemption_03':
-            return SoundAlert.POINT_REDEMPTION_03
-        elif jsonString == 'point_redemption_04':
-            return SoundAlert.POINT_REDEMPTION_04
-        elif jsonString == 'point_redemption_05':
-            return SoundAlert.POINT_REDEMPTION_05
-        elif jsonString == 'point_redemption_06':
-            return SoundAlert.POINT_REDEMPTION_06
-        elif jsonString == 'point_redemption_07':
-            return SoundAlert.POINT_REDEMPTION_07
-        elif jsonString == 'point_redemption_08':
-            return SoundAlert.POINT_REDEMPTION_08
-        elif jsonString == 'point_redemption_09':
-            return SoundAlert.POINT_REDEMPTION_09
-        elif jsonString == 'point_redemption_10':
-            return SoundAlert.POINT_REDEMPTION_10
-        elif jsonString == 'point_redemption_11':
-            return SoundAlert.POINT_REDEMPTION_11
-        elif jsonString == 'point_redemption_12':
-            return SoundAlert.POINT_REDEMPTION_12
-        elif jsonString == 'point_redemption_13':
-            return SoundAlert.POINT_REDEMPTION_13
-        elif jsonString == 'point_redemption_14':
-            return SoundAlert.POINT_REDEMPTION_14
-        elif jsonString == 'point_redemption_15':
-            return SoundAlert.POINT_REDEMPTION_15
-        elif jsonString == 'point_redemption_16':
-            return SoundAlert.POINT_REDEMPTION_16
-        elif jsonString == 'raid':
-            return SoundAlert.RAID
-        elif jsonString == 'subscribe':
-            return SoundAlert.SUBSCRIBE
-        else:
-            self.__timber.log('SoundAlertJsonMapper', f'Encountered unknown SoundAlert value: \"{jsonString}\"')
-            return None
+        match jsonString:
+            case 'cheer': return SoundAlert.CHEER
+            case 'point_redemption_01': return SoundAlert.POINT_REDEMPTION_01
+            case 'point_redemption_02': return SoundAlert.POINT_REDEMPTION_02
+            case 'point_redemption_03': return SoundAlert.POINT_REDEMPTION_03
+            case 'point_redemption_04': return SoundAlert.POINT_REDEMPTION_04
+            case 'point_redemption_05': return SoundAlert.POINT_REDEMPTION_05
+            case 'point_redemption_06': return SoundAlert.POINT_REDEMPTION_06
+            case 'point_redemption_07': return SoundAlert.POINT_REDEMPTION_07
+            case 'point_redemption_08': return SoundAlert.POINT_REDEMPTION_08
+            case 'point_redemption_09': return SoundAlert.POINT_REDEMPTION_09
+            case 'point_redemption_10': return SoundAlert.POINT_REDEMPTION_10
+            case 'point_redemption_11': return SoundAlert.POINT_REDEMPTION_11
+            case 'point_redemption_12': return SoundAlert.POINT_REDEMPTION_12
+            case 'point_redemption_13': return SoundAlert.POINT_REDEMPTION_13
+            case 'point_redemption_14': return SoundAlert.POINT_REDEMPTION_14
+            case 'point_redemption_15': return SoundAlert.POINT_REDEMPTION_15
+            case 'point_redemption_16': return SoundAlert.POINT_REDEMPTION_16
+            case 'raid': return SoundAlert.RAID
+            case 'subscribe': return SoundAlert.SUBSCRIBE
+            case _:
+                self.__timber.log('SoundAlertJsonMapper', f'Encountered unknown SoundAlert value: \"{jsonString}\"')
+                return None
