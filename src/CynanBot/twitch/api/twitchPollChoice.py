ab0a8171ce448b28a5514751348e72961ad50d91
Charles Madere <charlesrmadere@gmail.com>
2024-05-25 20:51:55 +0900

    `TwitchPollChoice` is now a frozen Python dataclass

--- src/CynanBot/twitch/api/twitchPollChoice.py
+++ src/CynanBot/twitch/api/twitchPollChoice.py
@@ -1,55 +1,9 @@
-from typing import Any
-
-import CynanBot.misc.utils as utils
+from dataclasses import dataclass
 
 
+@dataclass(frozen = True)
 class TwitchPollChoice():
-
-    def __init__(
-        self,
-        channelPointsVotes: int,
-        votes: int,
-        choiceId: str,
-        title: str
-    ):
-        if not utils.isValidInt(channelPointsVotes):
-            raise TypeError(f'channelPointsVotes argument is malformed: \"{channelPointsVotes}\"')
-        elif channelPointsVotes < 0 or channelPointsVotes > utils.getIntMaxSafeSize():
-            raise ValueError(f'channelPointsVotes argument is out of bounds: {channelPointsVotes}')
-        elif not utils.isValidInt(votes):
-            raise TypeError(f'votes argument is malformed: \"{votes}\"')
-        elif votes < 0 or votes > utils.getIntMaxSafeSize():
-            raise ValueError(f'votes argument is out of bounds: {votes}')
-        elif not utils.isValidStr(choiceId):
-            raise TypeError(f'choiceId argument is malformed: \"{choiceId}\"')
-        elif not utils.isValidStr(title):
-            raise TypeError(f'title argument is malformed: \"{title}\"')
-
-        self.__channelPointsVotes: int = channelPointsVotes
-        self.__votes: int = votes
-        self.__choiceId: str = choiceId
-        self.__title: str = title
-
-    def getChannelPointsVotes(self) -> int:
-        return self.__channelPointsVotes
-
-    def getChoiceId(self) -> str:
-        return self.__choiceId
-
-    def getTitle(self) -> str:
-        return self.__title
-
-    def getVotes(self) -> int:
-        return self.__votes
-
-    def __repr__(self) -> str:
-        dictionary = self.toDictionary()
-        return str(dictionary)
-
-    def toDictionary(self) -> dict[str, Any]:
-        return {
-            'channelPointsVotes': self.__channelPointsVotes,
-            'choiceId': self.__choiceId,
-            'title': self.__title,
-            'votes': self.__votes
-        }
+    channelPointsVotes: int
+    votes: int
+    choiceId: str
+    title: str
