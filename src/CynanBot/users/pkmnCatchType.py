655cbac9461fea40534d15c71eaddac70449c12a
Charles Madere <charlesrmadere@gmail.com>
2024-05-26 13:37:55 +0900

    removed an unused method

--- src/CynanBot/users/pkmnCatchType.py
+++ src/CynanBot/users/pkmnCatchType.py
@@ -8,13 +8,3 @@ class PkmnCatchType(EnumWithToFromStr):
     GREAT = auto()
     NORMAL = auto()
     ULTRA = auto()
-
-    def getSortOrder(self) -> int:
-        if self is PkmnCatchType.GREAT:
-            return 1
-        elif self is PkmnCatchType.NORMAL:
-            return 0
-        elif self is PkmnCatchType.ULTRA:
-            return 2
-        else:
-            raise RuntimeError(f'unknown PkmnCatchType: \"{self}\"')
