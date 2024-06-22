b6c31e8142150017d9ef462c1ae0b6ecc0df2341
Charles Madere <charlesrmadere@gmail.com>
2024-06-22 18:44:01 +0900

    further enhancing some debug info

--- src/CynanBot/recurringActions/wizards/absSteps.py
+++ src/CynanBot/recurringActions/wizards/absSteps.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 from CynanBot.recurringActions.wizards.absStep import AbsStep
 from CynanBot.recurringActions.wizards.stepResult import StepResult
@@ -13,3 +14,12 @@ class AbsSteps(ABC):
     @abstractmethod
     def stepForward(self) -> StepResult:
         pass
+
+    def __repr__(self) -> str:
+        dictionary = self.toDictionary()
+        return str(dictionary)
+
+    def toDictionary(self) -> dict[str, Any]:
+        return {
+            'step': self.getStep()
+        }
