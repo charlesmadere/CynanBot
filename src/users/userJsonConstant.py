aa2e0e5141c8ec5ccd957c498b553f677ce8e91f
Charles Madere <charlesrmadere@gmail.com>
2024-07-24 22:45:32 +0900

    beginning of constants for `UsersRepository` json keys

---
+++ src/users/userJsonConstant.py
@@ -0,0 +1,48 @@
+from enum import Enum, auto
+
+
+class UserJsonConstant(Enum):
+
+    ANIV_CONTENT_SCANNING_ENABLED = auto()
+    ANIV_MESSAGE_COPY_TIMEOUT_ENABLED = auto()
+    BEAN_CHANCES_ENABLED = auto()
+    CAT_JAM_MESSAGE_ENABLED = auto()
+    CHEER_ACTIONS_ENABLED = auto()
+    CUTENESS_ENABLED = auto()
+    CYNAN_SOURCE_ENABLED = auto()
+    ENABLED = auto()
+    GIVE_CUTENESS_ENABLED = auto()
+    LOCATION_ID = auto()
+    RAT_JAM_MESSAGE_ENABLED = auto()
+    RECURRING_ACTIONS_ENABLED = auto()
+    SOUND_ALERTS_ENABLED = auto()
+    TIMEOUT_CHEER_ACTIONS_ENABLED = auto()
+    TIMEZONE = auto()
+    TIMEZONES = auto()
+    TTS_ENABLED = auto()
+    TWITTER_URL = auto()
+    WEATHER_ENABLED = auto()
+
+    @property
+    def jsonKey(self) -> str:
+        match self:
+            case UserJsonConstant.ANIV_CONTENT_SCANNING_ENABLED: return 'anivContentScanningEnabled'
+            case UserJsonConstant.ANIV_MESSAGE_COPY_TIMEOUT_ENABLED: return 'anivMessageCopyTimeoutEnabled'
+            case UserJsonConstant.BEAN_CHANCES_ENABLED: return 'beanChancesEnabled'
+            case UserJsonConstant.CAT_JAM_MESSAGE_ENABLED: return 'catJamMessageEnabled'
+            case UserJsonConstant.CHEER_ACTIONS_ENABLED: return 'cheerActionsEnabled'
+            case UserJsonConstant.CUTENESS_ENABLED: return 'cutenessEnabled'
+            case UserJsonConstant.CYNAN_SOURCE_ENABLED: return 'cynanSourceEnabled'
+            case UserJsonConstant.ENABLED: return 'enabled'
+            case UserJsonConstant.GIVE_CUTENESS_ENABLED: return 'giveCutenessEnabled'
+            case UserJsonConstant.LOCATION_ID: return 'locationId'
+            case UserJsonConstant.RAT_JAM_MESSAGE_ENABLED: return 'ratJamMessageEnabled'
+            case UserJsonConstant.RECURRING_ACTIONS_ENABLED: return 'recurringActionsEnabled'
+            case UserJsonConstant.SOUND_ALERTS_ENABLED: return 'soundAlertsEnabled'
+            case UserJsonConstant.TIMEOUT_CHEER_ACTIONS_ENABLED: return 'timeoutCheerActionsEnabled'
+            case UserJsonConstant.TIMEZONE: return 'timeZone'
+            case UserJsonConstant.TIMEZONES: return 'timeZones'
+            case UserJsonConstant.TTS_ENABLED: return 'ttsEnabled'
+            case UserJsonConstant.TWITTER_URL: return 'twitterUrl'
+            case UserJsonConstant.WEATHER_ENABLED: return 'weatherEnabled'
+            case _: raise ValueError(f'unknown UserJsonConstant value: \"{self}\"')
---
+++ tests/users/test_userJsonConstants.py
@@ -0,0 +1,80 @@
+from src.users.userJsonConstant import UserJsonConstant
+
+
+class TestUserJsonConstants:
+
+    def test_jsonKey_withAnivContentScanningEnabled(self):
+        result = UserJsonConstant.ANIV_CONTENT_SCANNING_ENABLED.jsonKey
+        assert result == 'anivContentScanningEnabled'
+
+    def test_jsonKey_withAnivMessageCopyTimeoutEnabled(self):
+        result = UserJsonConstant.ANIV_MESSAGE_COPY_TIMEOUT_ENABLED.jsonKey
+        assert result == 'anivMessageCopyTimeoutEnabled'
+
+    def test_jsonKey_withBeanChancesEnabled(self):
+        result = UserJsonConstant.BEAN_CHANCES_ENABLED.jsonKey
+        assert result == 'beanChancesEnabled'
+
+    def test_jsonKey_withCatJamMessageEnabled(self):
+        result = UserJsonConstant.CAT_JAM_MESSAGE_ENABLED.jsonKey
+        assert result == 'catJamMessageEnabled'
+
+    def test_jsonKey_withCheerActionsEnabled(self):
+        result = UserJsonConstant.CHEER_ACTIONS_ENABLED.jsonKey
+        assert result == 'cheerActionsEnabled'
+
+    def test_jsonKey_withCutenessEnabled(self):
+        result = UserJsonConstant.CUTENESS_ENABLED.jsonKey
+        assert result == 'cutenessEnabled'
+
+    def test_jsonKey_withCynanSourceEnabled(self):
+        result = UserJsonConstant.CYNAN_SOURCE_ENABLED.jsonKey
+        assert result == 'cynanSourceEnabled'
+
+    def test_jsonKey_withEnabled(self):
+        result = UserJsonConstant.ENABLED.jsonKey
+        assert result == 'enabled'
+
+    def test_jsonKey_withGiveCutenessEnabled(self):
+        result = UserJsonConstant.GIVE_CUTENESS_ENABLED.jsonKey
+        assert result == 'giveCutenessEnabled'
+
+    def test_jsonKey_withLocationId(self):
+        result = UserJsonConstant.LOCATION_ID.jsonKey
+        assert result == 'locationId'
+
+    def test_jsonKey_withRatJamMessageEnabled(self):
+        result = UserJsonConstant.RAT_JAM_MESSAGE_ENABLED.jsonKey
+        assert result == 'ratJamMessageEnabled'
+
+    def test_jsonKey_withRecurringActionsEnabled(self):
+        result = UserJsonConstant.RECURRING_ACTIONS_ENABLED.jsonKey
+        assert result == 'recurringActionsEnabled'
+
+    def test_jsonKey_withSoundAlertsEnabled(self):
+        result = UserJsonConstant.SOUND_ALERTS_ENABLED.jsonKey
+        assert result == 'soundAlertsEnabled'
+
+    def test_jsonKey_withTimeoutCheerActionsEnabled(self):
+        result = UserJsonConstant.TIMEOUT_CHEER_ACTIONS_ENABLED.jsonKey
+        assert result == 'timeoutCheerActionsEnabled'
+
+    def test_jsonKey_withTimeZone(self):
+        result = UserJsonConstant.TIMEZONE.jsonKey
+        assert result == 'timeZone'
+
+    def test_jsonKey_withTimeZones(self):
+        result = UserJsonConstant.TIMEZONES.jsonKey
+        assert result == 'timeZones'
+
+    def test_jsonKey_withTtsEnabled(self):
+        result = UserJsonConstant.TTS_ENABLED.jsonKey
+        assert result == 'ttsEnabled'
+
+    def test_jsonKey_withTwitterUrl(self):
+        result = UserJsonConstant.TWITTER_URL.jsonKey
+        assert result == 'twitterUrl'
+
+    def test_jsonKey_withWeatherEnabled(self):
+        result = UserJsonConstant.WEATHER_ENABLED.jsonKey
+        assert result == 'weatherEnabled'
