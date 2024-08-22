bb626293986f68979ce82547e1091af65af2c3e2
Charles Madere <charlesrmadere@gmail.com>
2024-08-23 02:13:28 +0900

    wip

--- src/trivia/triviaRepositories/triviaDatabase/triviaDatabaseQuestionStorage.py
+++ src/trivia/triviaRepositories/triviaDatabase/triviaDatabaseQuestionStorage.py
@@ -22,7 +22,41 @@ class TriviaDatabaseQuestionStorage(TriviaDatabaseQuestionStorageInterface):
         self.__hasQuestionSetAvailable: bool | None = None
 
     async def fetchTriviaQuestion(self) -> TriviaDatabaseTriviaQuestion:
-        raise RuntimeError()
+        if not await aiofiles.ospath.exists(self.__triviaDatabaseFile):
+            raise FileNotFoundError(f'Trivia Database trivia database file not found: \"{self.__triviaDatabaseFile}\"')
+
+        connection = await aiosqlite.connect(self.__triviaDatabaseFile)
+        cursor = await connection.execute(
+            '''
+                SELECT category, correctAnswer, difficulty, question, questionId, triviaType, wrongAnswer1, wrongAnswer2, wrongAnswer3 FROM tdQuestions
+                ORDER BY RANDOM()
+                LIMIT 1
+            '''
+        )
+
+        row = await cursor.fetchone()
+        if not utils.hasItems(row) or len(row) != 9:
+            raise RuntimeError(f'Received malformed data from {self.triviaSource} database: {row}')
+
+        triviaQuestionDict: dict[str, Any] = {
+            'category': row[0],
+            'correctAnswer': row[1],
+            'difficulty': row[2],
+            'question': row[3],
+            'triviaId': row[4],
+            'type': row[5],
+            'wrongAnswers': [ row[6], row[7], row[8] ]
+        }
+
+        await cursor.close()
+        await connection.close()
+        return triviaQuestionDict
 
     async def hasQuestionSetAvailable(self) -> bool:
-        raise RuntimeError()
+        if self.__hasQuestionSetAvailable is not None:
+            return self.__hasQuestionSetAvailable
+
+        hasQuestionSetAvailable = await aiofiles.ospath.exists(self.__triviaDatabaseFile)
+        self.__hasQuestionSetAvailable = hasQuestionSetAvailable
+
+        return hasQuestionSetAvailable
