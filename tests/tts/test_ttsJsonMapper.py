3be060129fbcecc5cc4822440370c7cc8fbdec45
Charles Madere <charlesrmadere@gmail.com>
2024-10-16 20:30:22 +0900

    fixed some busted tests

--- tests/tts/test_ttsJsonMapper.py
+++ tests/tts/test_ttsJsonMapper.py
@@ -30,39 +30,39 @@ class TestTtsJsonMapper:
         result = await self.jsonMapper.asyncSerializeProvider(TtsProvider.DEC_TALK)
         assert result == 'dec_talk'
 
-    async def test_parseProvider_withDecTalkString(self):
+    def test_parseProvider_withDecTalkString(self):
         result = self.jsonMapper.parseProvider('dec_talk')
         assert result is TtsProvider.DEC_TALK
 
-    async def test_parseProvider_withEmptyString(self):
+    def test_parseProvider_withEmptyString(self):
         result = self.jsonMapper.parseProvider('')
         assert result is None
 
-    async def test_parseProvider_withGoogleString(self):
+    def test_parseProvider_withGoogleString(self):
         result = self.jsonMapper.parseProvider('google')
         assert result is TtsProvider.GOOGLE
 
-    async def test_parseProvider_withNone(self):
+    def test_parseProvider_withNone(self):
         result = self.jsonMapper.parseProvider(None)
         assert result is None
 
-    async def test_parseProvider_withStreamElementsString(self):
+    def test_parseProvider_withStreamElementsString(self):
         result = self.jsonMapper.parseProvider('stream_elements')
         assert result is TtsProvider.STREAM_ELEMENTS
 
-    async def test_parseProvider_withTtsMonsterString(self):
-        result = self.jsonMapper.parseProvider('ttsmonster')
+    def test_parseProvider_withTtsMonsterString(self):
+        result = self.jsonMapper.parseProvider('tts_monster')
         assert result is TtsProvider.TTS_MONSTER
 
-    async def test_parseProvider_withWhitespaceString(self):
+    def test_parseProvider_withWhitespaceString(self):
         result = self.jsonMapper.parseProvider(' ')
         assert result is None
 
-    async def test_requireProvider_withDecTalkString(self):
+    def test_requireProvider_withDecTalkString(self):
         result = self.jsonMapper.requireProvider('dec_talk')
         assert result is TtsProvider.DEC_TALK
 
-    async def test_requireProvider_withEmptyString(self):
+    def test_requireProvider_withEmptyString(self):
         result: TtsProvider | None = None
 
         with pytest.raises(ValueError):
@@ -70,11 +70,11 @@ class TestTtsJsonMapper:
 
         assert result is None
 
-    async def test_requireProvider_withGoogleString(self):
+    def test_requireProvider_withGoogleString(self):
         result = self.jsonMapper.requireProvider('google')
         assert result is TtsProvider.GOOGLE
 
-    async def test_requireProvider_withNone(self):
+    def test_requireProvider_withNone(self):
         result: TtsProvider | None = None
 
         with pytest.raises(ValueError):
@@ -82,11 +82,11 @@ class TestTtsJsonMapper:
 
         assert result is None
 
-    async def test_requireProvider_withTtsMonsterString(self):
+    def test_requireProvider_withTtsMonsterString(self):
         result = self.jsonMapper.requireProvider('tts_monster')
         assert result is TtsProvider.TTS_MONSTER
 
-    async def test_requireProvider_withWhitespaceString(self):
+    def test_requireProvider_withWhitespaceString(self):
         result: TtsProvider | None = None
 
         with pytest.raises(ValueError):
@@ -94,18 +94,18 @@ class TestTtsJsonMapper:
 
         assert result is None
 
-    async def test_serializeProvider_withDecTalk(self):
+    def test_serializeProvider_withDecTalk(self):
         result = self.jsonMapper.serializeProvider(TtsProvider.DEC_TALK)
         assert result == 'dec_talk'
 
-    async def test_serializeProvider_withGoogle(self):
+    def test_serializeProvider_withGoogle(self):
         result = self.jsonMapper.serializeProvider(TtsProvider.GOOGLE)
         assert result == 'google'
 
-    async def test_serializeProvider_withStreamElements(self):
+    def test_serializeProvider_withStreamElements(self):
         result = self.jsonMapper.serializeProvider(TtsProvider.STREAM_ELEMENTS)
         assert result == 'stream_elements'
 
-    async def test_serializeProvider_withTtsMonster(self):
+    def test_serializeProvider_withTtsMonster(self):
         result = self.jsonMapper.serializeProvider(TtsProvider.TTS_MONSTER)
         assert result == 'tts_monster'
