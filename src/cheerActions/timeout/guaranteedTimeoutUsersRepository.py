e2d8d279ba300adee6a72a6d7dec8bb92e7ef0df
Charles Madere <charlesrmadere@gmail.com>
2024-10-26 16:44:00 +0900

    filled in basic functionality for `GuaranteedTimeoutUsersRepository`

--- src/cheerActions/timeout/guaranteedTimeoutUsersRepository.py
+++ src/cheerActions/timeout/guaranteedTimeoutUsersRepository.py
@@ -1,5 +1,6 @@
 from .guaranteedTimeoutUsersRepositoryInterface import GuaranteedTimeoutUsersRepositoryInterface
 from ...aniv.anivUserIdProviderInterface import AnivUserIdProviderInterface
+from ...misc import utils as utils
 
 
 class GuaranteedTimeoutUsersRepository(GuaranteedTimeoutUsersRepositoryInterface):
@@ -13,5 +14,21 @@ class GuaranteedTimeoutUsersRepository(GuaranteedTimeoutUsersRepositoryInterface
 
         self.__anivUserIdProvider: AnivUserIdProviderInterface = anivUserIdProvider
 
-    async def getUserIds(self):
-        return frozenset()
+        self.__userIds: frozenset[str] | None = None
+
+    async def __createUserIdsSet(self) -> frozenset[str]:
+        userIds: set[str] = set()
+
+        anivUserId = await self.__anivUserIdProvider.getAnivUserId()
+        if utils.isValidStr(anivUserId):
+            userIds.add(anivUserId)
+
+        return frozenset(userIds)
+
+    async def getUserIds(self) -> frozenset[str]:
+        userIds = self.__userIds
+
+        if userIds is None:
+            userIds = await self.__createUserIdsSet()
+
+        return userIds
