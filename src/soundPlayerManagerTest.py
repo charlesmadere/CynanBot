bd5629a3806bc77a086f9cd1553a0f2d2bf26fbc
Charles Madere <charlesrmadere@gmail.com>
2025-01-18 11:44:11 +0900

    `soundPlayerManagerTest` now works for different sound player manager implementations

--- src/soundPlayerManagerTest.py
+++ src/soundPlayerManagerTest.py
@@ -1,8 +1,6 @@
 import asyncio
 from asyncio import AbstractEventLoop
 
-from .aniv.anivContentScanner import AnivContentScanner
-from .aniv.anivContentScannerInterface import AnivContentScannerInterface
 from .chatBand.chatBandInstrumentSoundsRepository import ChatBandInstrumentSoundsRepository
 from .chatBand.chatBandInstrumentSoundsRepositoryInterface import ChatBandInstrumentSoundsRepositoryInterface
 from .contentScanner.bannedWordsRepository import BannedWordsRepository
@@ -11,19 +9,20 @@ from .contentScanner.contentScanner import ContentScanner
 from .contentScanner.contentScannerInterface import ContentScannerInterface
 from .misc.backgroundTaskHelper import BackgroundTaskHelper
 from .misc.backgroundTaskHelperInterface import BackgroundTaskHelperInterface
+from .soundPlayerManager.audioPlayer.audioPlayerSoundPlayerManagerProvider import AudioPlayerSoundPlayerManagerProvider
 from .soundPlayerManager.soundAlert import SoundAlert
-from .soundPlayerManager.soundPlayerManagerInterface import SoundPlayerManagerInterface
+from .soundPlayerManager.soundPlayerManagerProviderInterface import SoundPlayerManagerProviderInterface
 from .soundPlayerManager.soundPlayerSettingsRepository import SoundPlayerSettingsRepository
 from .soundPlayerManager.soundPlayerSettingsRepositoryInterface import SoundPlayerSettingsRepositoryInterface
-from .soundPlayerManager.vlc.vlcSoundPlayerManager import VlcSoundPlayerManager
+from .soundPlayerManager.soundPlayerType import SoundPlayerType
+from .soundPlayerManager.stub.stubSoundPlayerManagerProvider import StubSoundPlayerManagerProvider
+from .soundPlayerManager.vlc.vlcSoundPlayerManagerProvider import VlcSoundPlayerManagerProvider
 from .storage.jsonStaticReader import JsonStaticReader
 from .storage.linesStaticReader import LinesStaticReader
 from .timber.timberInterface import TimberInterface
 from .timber.timberStub import TimberStub
-from .trivia.compilers.triviaAnswerCompiler import TriviaAnswerCompiler
-from .trivia.compilers.triviaAnswerCompilerInterface import TriviaAnswerCompilerInterface
-from .trivia.triviaSettingsRepository import TriviaSettingsRepository
-from .trivia.triviaSettingsRepositoryInterface import TriviaSettingsRepositoryInterface
+
+soundPlayerType = SoundPlayerType.VLC
 
 eventLoop: AbstractEventLoop = asyncio.new_event_loop()
 asyncio.set_event_loop(eventLoop)
@@ -51,31 +50,36 @@ chatBandInstrumentSoundsRepository: ChatBandInstrumentSoundsRepositoryInterface 
     timber = timber
 )
 
-anivContentScanner: AnivContentScannerInterface = AnivContentScanner(
-    contentScanner = contentScanner,
-    timber = timber
-)
-
-triviaSettingsRepository: TriviaSettingsRepositoryInterface = TriviaSettingsRepository(
-    settingsJsonReader = JsonStaticReader(dict())
-)
-
-triviaAnswerCompiler: TriviaAnswerCompilerInterface = TriviaAnswerCompiler(
-    timber = timber
-)
-
 soundPlayerSettingsRepository: SoundPlayerSettingsRepositoryInterface = SoundPlayerSettingsRepository(
     settingsJsonReader = JsonStaticReader(dict())
 )
 
-soundPlayerManager: SoundPlayerManagerInterface = VlcSoundPlayerManager(
-    chatBandInstrumentSoundsRepository = chatBandInstrumentSoundsRepository,
-    soundPlayerSettingsRepository = soundPlayerSettingsRepository,
-    timber = timber
-)
+soundPlayerManagerProvider: SoundPlayerManagerProviderInterface
+
+match soundPlayerType:
+    case SoundPlayerType.AUDIO_PLAYER:
+        soundPlayerManagerProvider = AudioPlayerSoundPlayerManagerProvider(
+            chatBandInstrumentSoundsRepository = None,
+            soundPlayerSettingsRepository = soundPlayerSettingsRepository,
+            timber = timber
+        )
+
+    case SoundPlayerType.STUB:
+        soundPlayerManagerProvider = StubSoundPlayerManagerProvider()
+
+    case SoundPlayerType.VLC:
+        soundPlayerManagerProvider = VlcSoundPlayerManagerProvider(
+            chatBandInstrumentSoundsRepository = None,
+            soundPlayerSettingsRepository = soundPlayerSettingsRepository,
+            timber = timber
+        )
+
+    case _:
+        raise RuntimeError(f'Unknown/misconfigured SoundPlayerType: \"{soundPlayerType}\"')
 
 async def main():
     pass
+    soundPlayerManager = soundPlayerManagerProvider.constructNewSoundPlayerManagerInstance()
     await soundPlayerManager.playSoundAlert(SoundAlert.SUBSCRIBE)
     print('sleep')
     await asyncio.sleep(10)
