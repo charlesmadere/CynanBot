4ce1bae8149e2ad6763271fcb55d90538d1f5763
Melinda Kerrigan <168405131+mintymelinda@users.noreply.github.com>
2025-02-07 09:26:49 -0500

    actually fixed subscription checking

--- src/twitch/subscribers/twitchSubscriptionsRepositoryInterface.py
+++ src/twitch/subscribers/twitchSubscriptionsRepositoryInterface.py
@@ -7,6 +7,15 @@ from ...misc.clearable import Clearable
 class TwitchSubscriptionsRepositoryInterface(Clearable):
 
     @abstractmethod
+    async def fetchBroadcasterSubscription(
+        self,
+        twitchAccessToken: str,
+        twitchChannelId: str,
+        userId: str
+    ) -> TwitchSubscriptionStatus | None:
+        pass
+
+    @abstractmethod
     async def fetchSubscriptionStatus(
         self,
         twitchAccessToken: str,
