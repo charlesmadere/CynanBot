8af1ca2c089b672713d2a6566614753f5ff2d66d
Charles Madere <charlesrmadere@gmail.com>
2025-02-16 20:11:44 +0900

    added a few more languages

--- src/google/models/googleVoicePreset.py
+++ src/google/models/googleVoicePreset.py
@@ -18,10 +18,19 @@ class GoogleVoicePreset(Enum):
     ITALIAN_ITALY_CHIRP_D = auto()
     ITALIAN_ITALY_CHIRP_F = auto()
     ITALIAN_ITALY_CHIRP_O = auto()
+    JAPANESE_JAPAN_STANDARD_A = auto()
+    JAPANESE_JAPAN_STANDARD_B = auto()
+    JAPANESE_JAPAN_STANDARD_C = auto()
+    JAPANESE_JAPAN_STANDARD_D = auto()
     KOREAN_KOREA_STANDARD_A = auto()
     KOREAN_KOREA_STANDARD_B = auto()
     KOREAN_KOREA_STANDARD_C = auto()
     KOREAN_KOREA_STANDARD_D = auto()
+    PORTUGUESE_BRAZIL_STANDARD_A = auto()
+    PORTUGUESE_BRAZIL_STANDARD_B = auto()
+    PORTUGUESE_BRAZIL_STANDARD_C = auto()
+    PORTUGUESE_BRAZIL_STANDARD_D = auto()
+    PORTUGUESE_BRAZIL_STANDARD_E = auto()
     SWEDISH_SWEDEN_STANDARD_A = auto()
     SWEDISH_SWEDEN_STANDARD_B = auto()
     SWEDISH_SWEDEN_STANDARD_C = auto()
@@ -45,10 +54,19 @@ class GoogleVoicePreset(Enum):
             case GoogleVoicePreset.ITALIAN_ITALY_CHIRP_D: return 'it-IT-Chirp-HD-D'
             case GoogleVoicePreset.ITALIAN_ITALY_CHIRP_F: return 'it-IT-Chirp-HD-F'
             case GoogleVoicePreset.ITALIAN_ITALY_CHIRP_O: return 'it-IT-Chirp-HD-O'
+            case GoogleVoicePreset.JAPANESE_JAPAN_STANDARD_A: return 'ja-JP-Standard-A'
+            case GoogleVoicePreset.JAPANESE_JAPAN_STANDARD_B: return 'ja-JP-Standard-B'
+            case GoogleVoicePreset.JAPANESE_JAPAN_STANDARD_C: return 'ja-JP-Standard-C'
+            case GoogleVoicePreset.JAPANESE_JAPAN_STANDARD_D: return 'ja-JP-Standard-D'
             case GoogleVoicePreset.KOREAN_KOREA_STANDARD_A: return 'ko-KR-Standard-A'
             case GoogleVoicePreset.KOREAN_KOREA_STANDARD_B: return 'ko-KR-Standard-B'
             case GoogleVoicePreset.KOREAN_KOREA_STANDARD_C: return 'ko-KR-Standard-C'
             case GoogleVoicePreset.KOREAN_KOREA_STANDARD_D: return 'ko-KR-Standard-D'
+            case GoogleVoicePreset.PORTUGUESE_BRAZIL_STANDARD_A: return 'pt-BR-Standard-A'
+            case GoogleVoicePreset.PORTUGUESE_BRAZIL_STANDARD_B: return 'pt-BR-Standard-B'
+            case GoogleVoicePreset.PORTUGUESE_BRAZIL_STANDARD_C: return 'pt-BR-Standard-C'
+            case GoogleVoicePreset.PORTUGUESE_BRAZIL_STANDARD_D: return 'pt-BR-Standard-D'
+            case GoogleVoicePreset.PORTUGUESE_BRAZIL_STANDARD_E: return 'pt-BR-Standard-E'
             case GoogleVoicePreset.SWEDISH_SWEDEN_STANDARD_A: return 'sv-SE-Standard-A'
             case GoogleVoicePreset.SWEDISH_SWEDEN_STANDARD_B: return 'sv-SE-Standard-B'
             case GoogleVoicePreset.SWEDISH_SWEDEN_STANDARD_C: return 'sv-SE-Standard-C'
@@ -73,10 +91,19 @@ class GoogleVoicePreset(Enum):
             case GoogleVoicePreset.ITALIAN_ITALY_CHIRP_D: return 'it-IT'
             case GoogleVoicePreset.ITALIAN_ITALY_CHIRP_F: return 'it-IT'
             case GoogleVoicePreset.ITALIAN_ITALY_CHIRP_O: return 'it-IT'
+            case GoogleVoicePreset.JAPANESE_JAPAN_STANDARD_A: return 'ja-JP'
+            case GoogleVoicePreset.JAPANESE_JAPAN_STANDARD_B: return 'ja-JP'
+            case GoogleVoicePreset.JAPANESE_JAPAN_STANDARD_C: return 'ja-JP'
+            case GoogleVoicePreset.JAPANESE_JAPAN_STANDARD_D: return 'ja-JP'
             case GoogleVoicePreset.KOREAN_KOREA_STANDARD_A: return 'ko-KR'
             case GoogleVoicePreset.KOREAN_KOREA_STANDARD_B: return 'ko-KR'
             case GoogleVoicePreset.KOREAN_KOREA_STANDARD_C: return 'ko-KR'
             case GoogleVoicePreset.KOREAN_KOREA_STANDARD_D: return 'ko-KR'
+            case GoogleVoicePreset.PORTUGUESE_BRAZIL_STANDARD_A: return 'pt-BR'
+            case GoogleVoicePreset.PORTUGUESE_BRAZIL_STANDARD_B: return 'pt-BR'
+            case GoogleVoicePreset.PORTUGUESE_BRAZIL_STANDARD_C: return 'pt-BR'
+            case GoogleVoicePreset.PORTUGUESE_BRAZIL_STANDARD_D: return 'pt-BR'
+            case GoogleVoicePreset.PORTUGUESE_BRAZIL_STANDARD_E: return 'pt-BR'
             case GoogleVoicePreset.SWEDISH_SWEDEN_STANDARD_A: return 'sv-SE'
             case GoogleVoicePreset.SWEDISH_SWEDEN_STANDARD_B: return 'sv-SE'
             case GoogleVoicePreset.SWEDISH_SWEDEN_STANDARD_C: return 'sv-SE'
