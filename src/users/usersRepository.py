433f36d492dc4dc065b98b47a4e30222eeb0ebc6
Charles Madere <mcharles@mercari.com>
2025-02-21 19:05:52 +0900

    `chatterPreferredTtsEnabled` should default to `False`

--- src/users/usersRepository.py
+++ src/users/usersRepository.py
@@ -164,7 +164,7 @@ class UsersRepository(UsersRepositoryInterface):
         isChatBackMessagesEnabled = utils.getBoolFromDict(userJson, 'chatBackMessagesEnabled', False)
         isChatBandEnabled = utils.getBoolFromDict(userJson, 'chatBandEnabled', False)
         isChatLoggingEnabled = utils.getBoolFromDict(userJson, 'chatLoggingEnabled', False)
-        isChatterPreferredTtsEnabled = utils.getBoolFromDict(userJson, 'chatterPreferredTtsEnabled', True)
+        isChatterPreferredTtsEnabled = utils.getBoolFromDict(userJson, 'chatterPreferredTtsEnabled', False)
         isCrowdControlEnabled = utils.getBoolFromDict(userJson, UserJsonConstant.CROWD_CONTROL_ENABLED.jsonKey, False)
         isCutenessEnabled = utils.getBoolFromDict(userJson, 'cutenessEnabled', False)
         isDecTalkSongsEnabled = utils.getBoolFromDict(userJson, 'decTalkSongsEnabled', False)
