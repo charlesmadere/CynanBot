81136ea4a2348a55e36b6adc7f4880cb261300a3
Charles Madere <charlesrmadere@gmail.com>
2025-03-23 17:53:34 +0900

    added Twitch user ID for `a_n_e_e_v`

--- src/aniv/anivUserIdProviderInterface.py
+++ src/aniv/anivUserIdProviderInterface.py
@@ -4,5 +4,9 @@ from abc import ABC, abstractmethod
 class AnivUserIdProviderInterface(ABC):
 
     @abstractmethod
+    async def getAneevUserId(self) -> str | None:
+        pass
+
+    @abstractmethod
     async def getAnivUserId(self) -> str | None:
         pass
