fcd6ef7724272331511456f881d17b216ada582f
Charles Madere <charlesrmadere@gmail.com>
2025-03-24 02:53:56 +0900

    removed `AnivUserIdProviderInterface#getAneevUserId()` method

--- src/aniv/anivUserIdProviderInterface.py
+++ src/aniv/anivUserIdProviderInterface.py
@@ -4,9 +4,5 @@ from abc import ABC, abstractmethod
 class AnivUserIdProviderInterface(ABC):
 
     @abstractmethod
-    async def getAneevUserId(self) -> str | None:
-        pass
-
-    @abstractmethod
     async def getAnivUserId(self) -> str | None:
         pass
