520b9aa0dd057ed49af6fb28c1a9ccfdbd0d014e
Charles Madere <charlesrmadere@gmail.com>
2025-06-12 02:26:47 +0900

    slightly reduced stream elements volume

--- src/streamElements/settings/streamElementsSettingsRepository.py
+++ src/streamElements/settings/streamElementsSettingsRepository.py
@@ -52,7 +52,7 @@ class StreamElementsSettingsRepository(StreamElementsSettingsRepositoryInterface
 
     async def getMediaPlayerVolume(self) -> int | None:
         jsonContents = await self.__readJson()
-        return utils.getIntFromDict(jsonContents, 'media_player_volume', fallback = 20)
+        return utils.getIntFromDict(jsonContents, 'media_player_volume', fallback = 18)
 
     async def __readJson(self) -> dict[str, Any]:
         if self.__cache is not None:
