2f0686b9921067d8fb30fcbd758728ba474c29f3
Charles Madere <charlesrmadere@gmail.com>
2025-06-29 21:28:01 +0900

    added another lil method stub

--- src/tts/shotgun/shotgunTtsSettingsInterface.py
+++ src/tts/shotgun/shotgunTtsSettingsInterface.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
 
+from ..models.ttsProvider import TtsProvider
 from ...misc.clearable import Clearable
 
 
 class ShotgunTtsSettingsInterface(Clearable, ABC):
+
+    @abstractmethod
+    async def getEnabledTtsProviders(self) -> frozenset[TtsProvider]:
+        pass
 
     @abstractmethod
     async def getProviderCount(self) -> int | None:
