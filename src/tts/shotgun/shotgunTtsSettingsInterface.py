93a343a5413ea628d9cfd955e9ced49c316dc3c3
Charles Madere <charlesrmadere@gmail.com>
2025-06-29 21:08:22 +0900

    added another shotgun tts settings method

--- src/tts/shotgun/shotgunTtsSettingsInterface.py
+++ src/tts/shotgun/shotgunTtsSettingsInterface.py
@@ -6,5 +6,9 @@ from ...misc.clearable import Clearable
 class ShotgunTtsSettingsInterface(Clearable, ABC):
 
     @abstractmethod
+    async def getProviderCount(self) -> int | None:
+        pass
+
+    @abstractmethod
     async def isEnabled(self) -> bool:
         pass
