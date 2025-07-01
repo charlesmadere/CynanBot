d513863080b6ce3186f5f247f4afb4a6f21a6e3f
Charles Madere <charlesrmadere@gmail.com>
2025-07-02 01:06:01 +0900

    corrected a class's name

--- src/twitch/api/models/twitchConduitChallengeResponse.py
+++ src/twitch/api/models/twitchConduitChallengeResponse.py
@@ -6,6 +6,6 @@ from .twitchConduitShard import TwitchConduitShard
 # This class intends to directly correspond to Twitch's "Verifying a conduit" API:
 # https://dev.twitch.tv/docs/eventsub/handling-conduit-events/#verifying-a-conduit
 @dataclass(frozen = True)
-class TwitchConduitResponse:
+class TwitchConduitChallengeResponse:
     challenge: str
     conduitShard: TwitchConduitShard
