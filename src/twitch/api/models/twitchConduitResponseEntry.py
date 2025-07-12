7e7e00b105a38a1e913ed266cd1959120d18948c
Charles Madere <charlesrmadere@gmail.com>
2025-07-12 18:00:13 +0900

    small comment update

--- src/twitch/api/models/twitchConduitResponseEntry.py
+++ src/twitch/api/models/twitchConduitResponseEntry.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
 
 
-# This class intends to directly correspond to the "data" field from Twitch's "Creating
-# a Conduit" API: https://dev.twitch.tv/docs/eventsub/handling-conduit-events/#creating-a-conduit
+# This class intends to directly correspond to the "data" field from Twitch's "Creating a
+# Conduit" API: https://dev.twitch.tv/docs/eventsub/handling-conduit-events/#creating-a-conduit
 @dataclass(frozen = True)
 class TwitchConduitResponseEntry:
     shardCount: int
