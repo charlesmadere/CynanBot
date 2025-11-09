15ebed2bd928107294c364a7257f33695af60215
Charles Madere <charlesrmadere@gmail.com>
2025-11-08 18:51:31 -0800

    removed a few more `TwitchUtilsInterface` uses

--- src/chatCommands/deleteCheerActionChatCommand.py
+++ src/chatCommands/deleteCheerActionChatCommand.py
@@ -1,12 +1,13 @@
 import traceback
+from typing import Final
 
 from .absChatCommand import AbsChatCommand
 from ..cheerActions.cheerActionsRepositoryInterface import CheerActionsRepositoryInterface
 from ..misc import utils as utils
 from ..misc.administratorProviderInterface import AdministratorProviderInterface
 from ..timber.timberInterface import TimberInterface
+from ..twitch.chatMessenger.twitchChatMessengerInterface import TwitchChatMessengerInterface
 from ..twitch.configuration.twitchContext import TwitchContext
-from ..twitch.twitchUtilsInterface import TwitchUtilsInterface
 from ..users.userIdsRepositoryInterface import UserIdsRepositoryInterface
 from ..users.usersRepositoryInterface import UsersRepositoryInterface
 
@@ -18,9 +19,9 @@ class DeleteCheerActionChatCommand(AbsChatCommand):
         administratorProvider: AdministratorProviderInterface,
         cheerActionsRepository: CheerActionsRepositoryInterface,
         timber: TimberInterface,
-        twitchUtils: TwitchUtilsInterface,
+        twitchChatMessenger: TwitchChatMessengerInterface,
         userIdsRepository: UserIdsRepositoryInterface,
-        usersRepository: UsersRepositoryInterface
+        usersRepository: UsersRepositoryInterface,
     ):
         if not isinstance(administratorProvider, AdministratorProviderInterface):
             raise TypeError(f'administratorProvider argument is malformed: \"{administratorProvider}\"')
@@ -28,19 +29,19 @@ class DeleteCheerActionChatCommand(AbsChatCommand):
             raise TypeError(f'cheerActionsRepository argument is malformed: \"{cheerActionsRepository}\"')
         elif not isinstance(timber, TimberInterface):
             raise TypeError(f'timber argument is malformed: \"{timber}\"')
-        elif not isinstance(twitchUtils, TwitchUtilsInterface):
-            raise TypeError(f'twitchUtils argument is malformed: \"{twitchUtils}\"')
+        elif not isinstance(twitchChatMessenger, TwitchChatMessengerInterface):
+            raise TypeError(f'twitchChatMessenger argument is malformed: \"{twitchChatMessenger}\"')
         elif not isinstance(userIdsRepository, UserIdsRepositoryInterface):
             raise TypeError(f'userIdsRepository argument is malformed: \"{userIdsRepository}\"')
         elif not isinstance(usersRepository, UsersRepositoryInterface):
             raise TypeError(f'usersRepository argument is malformed: \"{usersRepository}\"')
 
-        self.__administratorProvider: AdministratorProviderInterface = administratorProvider
-        self.__cheerActionsRepository: CheerActionsRepositoryInterface = cheerActionsRepository
-        self.__timber: TimberInterface = timber
-        self.__twitchUtils: TwitchUtilsInterface = twitchUtils
-        self.__userIdsRepository: UserIdsRepositoryInterface = userIdsRepository
-        self.__usersRepository: UsersRepositoryInterface = usersRepository
+        self.__administratorProvider: Final[AdministratorProviderInterface] = administratorProvider
+        self.__cheerActionsRepository: Final[CheerActionsRepositoryInterface] = cheerActionsRepository
+        self.__timber: Final[TimberInterface] = timber
+        self.__twitchChatMessenger: Final[TwitchChatMessengerInterface] = twitchChatMessenger
+        self.__userIdsRepository: Final[UserIdsRepositoryInterface] = userIdsRepository
+        self.__usersRepository: Final[UsersRepositoryInterface] = usersRepository
 
     async def handleChatCommand(self, ctx: TwitchContext):
         user = await self.__usersRepository.getUserAsync(ctx.getTwitchChannelName())
@@ -58,25 +59,41 @@ class DeleteCheerActionChatCommand(AbsChatCommand):
         splits = utils.getCleanedSplits(ctx.getMessageContent())
         if len(splits) < 2 or not utils.strContainsAlphanumericCharacters(splits[1]):
             self.__timber.log('DeleteCheerActionCommand', f'Incorrect arguments given by {ctx.getAuthorName()}:{ctx.getAuthorId()} in {user.handle}')
-            await self.__twitchUtils.safeSend(ctx, f'⚠ Bit amount is necessary for the !deletecheeraction command. Example: !deletecheeraction 100')
+            self.__twitchChatMessenger.send(
+                text = f'⚠ Bit amount is necessary for the !deletecheeraction command. Example: !deletecheeraction 100',
+                twitchChannelId = await ctx.getTwitchChannelId(),
+                replyMessageId = await ctx.getMessageId(),
+            )
             return
 
         try:
             bits = int(splits[1])
         except Exception as e:
             self.__timber.log('DeleteCheerActionCommand', f'Cheer action was attempted to be deleted by {ctx.getAuthorName()}:{ctx.getAuthorId()} in {user.handle}, but was unable to convert: {e}', e, traceback.format_exc())
-            await self.__twitchUtils.safeSend(ctx, f'⚠ Bit amount is necessary for the !deletecheeraction command. Example: !deletecheeraction 100')
+            self.__twitchChatMessenger.send(
+                text = f'⚠ Bit amount is necessary for the !deletecheeraction command. Example: !deletecheeraction 100',
+                twitchChannelId = await ctx.getTwitchChannelId(),
+                replyMessageId = await ctx.getMessageId(),
+            )
             return
 
         action = await self.__cheerActionsRepository.deleteAction(
             bits = bits,
-            twitchChannelId = userId
+            twitchChannelId = userId,
         )
 
         if action is None:
             self.__timber.log('DeleteCheerActionCommand', f'Cheer {bits} was attempted to be deleted by {ctx.getAuthorName()}:{ctx.getAuthorId()} in {user.handle}, but no corresponding cheer action was found')
-            await self.__twitchUtils.safeSend(ctx, f'⚠ Could not find any corresponding cheer action with bits \"{bits}\"')
-            return
+            self.__twitchChatMessenger.send(
+                text = f'⚠ Could not find any corresponding cheer action with bits \"{bits}\"',
+                twitchChannelId = await ctx.getTwitchChannelId(),
+                replyMessageId = await ctx.getMessageId(),
+            )
+        else:
+            self.__twitchChatMessenger.send(
+                text = f'ⓘ Deleted cheer action — {action}',
+                twitchChannelId = await ctx.getTwitchChannelId(),
+                replyMessageId = await ctx.getMessageId(),
+            )
 
-        await self.__twitchUtils.safeSend(ctx, f'ⓘ Deleted cheer action — {action}')
-        self.__timber.log('DeleteCheerActionCommand', f'Handled !deletecheeraction command for {ctx.getAuthorName()}:{ctx.getAuthorId()} in {user.handle}')
+        self.__timber.log('DeleteCheerActionCommand', f'Handled command for {ctx.getAuthorName()}:{ctx.getAuthorId()} in {user.handle}')
