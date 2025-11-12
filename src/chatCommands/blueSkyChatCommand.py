8d693686102beca18819e60977a241965c6af8ad
Charles Madere <charlesrmadere@gmail.com>
2025-11-11 20:07:20 -0800

    removed a use of `TimedDict`

--- src/chatCommands/blueSkyChatCommand.py
+++ src/chatCommands/blueSkyChatCommand.py
@@ -1,9 +1,7 @@
-from datetime import timedelta
 from typing import Final
 
 from .absChatCommand import AbsChatCommand
 from ..misc import utils as utils
-from ..misc.timedDict import TimedDict
 from ..timber.timberInterface import TimberInterface
 from ..twitch.chatMessenger.twitchChatMessengerInterface import TwitchChatMessengerInterface
 from ..twitch.configuration.twitchContext import TwitchContext
@@ -17,7 +15,6 @@ class BlueSkyChatCommand(AbsChatCommand):
         timber: TimberInterface,
         twitchChatMessenger: TwitchChatMessengerInterface,
         usersRepository: UsersRepositoryInterface,
-        cooldown: timedelta = timedelta(minutes = 1),
     ):
         if not isinstance(timber, TimberInterface):
             raise TypeError(f'timber argument is malformed: \"{timber}\"')
@@ -25,21 +22,16 @@ class BlueSkyChatCommand(AbsChatCommand):
             raise TypeError(f'twitchChatMessenger argument is malformed: \"{twitchChatMessenger}\"')
         elif not isinstance(usersRepository, UsersRepositoryInterface):
             raise TypeError(f'usersRepository argument is malformed: \"{usersRepository}\"')
-        elif not isinstance(cooldown, timedelta):
-            raise TypeError(f'cooldown argument is malformed: \"{cooldown}\"')
 
         self.__timber: Final[TimberInterface] = timber
         self.__twitchChatMessenger: Final[TwitchChatMessengerInterface] = twitchChatMessenger
         self.__usersRepository: Final[UsersRepositoryInterface] = usersRepository
-        self.__lastMessageTimes: Final[TimedDict] = TimedDict(cooldown)
 
     async def handleChatCommand(self, ctx: TwitchContext):
         user = await self.__usersRepository.getUserAsync(ctx.getTwitchChannelName())
         blueSkyUrl = user.blueSkyUrl
 
         if not utils.isValidUrl(blueSkyUrl):
-            return
-        elif not ctx.isAuthorMod and not ctx.isAuthorVip and not self.__lastMessageTimes.isReadyAndUpdate(user.handle):
             return
 
         self.__twitchChatMessenger.send(
