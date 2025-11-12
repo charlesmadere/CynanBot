28d0c1b7dd2d474f95efa07fd72bd004345be763
Charles Madere <charlesrmadere@gmail.com>
2025-11-11 20:07:11 -0800

    removed another use of `TwitchUtilsInterface`

--- src/chatCommands/commandsChatCommand.py
+++ src/chatCommands/commandsChatCommand.py
@@ -1,11 +1,10 @@
-from datetime import timedelta
+from typing import Final
 
 from .absChatCommand import AbsChatCommand
 from ..misc import utils as utils
-from ..misc.timedDict import TimedDict
 from ..timber.timberInterface import TimberInterface
+from ..twitch.chatMessenger.twitchChatMessengerInterface import TwitchChatMessengerInterface
 from ..twitch.configuration.twitchContext import TwitchContext
-from ..twitch.twitchUtilsInterface import TwitchUtilsInterface
 from ..users.usersRepositoryInterface import UsersRepositoryInterface
 
 
@@ -14,37 +13,31 @@ class CommandsChatCommand(AbsChatCommand):
     def __init__(
         self,
         timber: TimberInterface,
-        twitchUtils: TwitchUtilsInterface,
+        twitchChatMessenger: TwitchChatMessengerInterface,
         usersRepository: UsersRepositoryInterface,
         commandsUrl: str = 'https://cynanbot.io',
-        cooldown: timedelta = timedelta(minutes = 2, seconds = 30),
     ):
         if not isinstance(timber, TimberInterface):
             raise TypeError(f'timber argument is malformed: \"{timber}\"')
-        elif not isinstance(twitchUtils, TwitchUtilsInterface):
-            raise TypeError(f'twitchUtils argument is malformed: \"{twitchUtils}\"')
+        elif not isinstance(twitchChatMessenger, TwitchChatMessengerInterface):
+            raise TypeError(f'twitchChatMessenger argument is malformed: \"{twitchChatMessenger}\"')
         elif not isinstance(usersRepository, UsersRepositoryInterface):
             raise TypeError(f'usersRepository argument is malformed: \"{usersRepository}\"')
         elif not utils.isValidUrl(commandsUrl):
             raise TypeError(f'commandsUrl argument is malformed: \"{commandsUrl}\"')
-        elif not isinstance(cooldown, timedelta):
-            raise TypeError(f'cooldown argument is malformed: \"{cooldown}\"')
 
-        self.__timber: TimberInterface = timber
-        self.__twitchUtils: TwitchUtilsInterface = twitchUtils
-        self.__usersRepository: UsersRepositoryInterface = usersRepository
-        self.__commandsUrl: str = commandsUrl
-        self.__lastMessageTimes: TimedDict = TimedDict(cooldown)
+        self.__timber: Final[TimberInterface] = timber
+        self.__twitchChatMessenger: Final[TwitchChatMessengerInterface] = twitchChatMessenger
+        self.__usersRepository: Final[UsersRepositoryInterface] = usersRepository
+        self.__commandsUrl: Final[str] = commandsUrl
 
     async def handleChatCommand(self, ctx: TwitchContext):
         user = await self.__usersRepository.getUserAsync(ctx.getTwitchChannelName())
-        if not ctx.isAuthorMod and not self.__lastMessageTimes.isReadyAndUpdate(user.handle):
-            return
 
-        await self.__twitchUtils.safeSend(
-            messageable = ctx,
-            message = f'ⓘ Commands: {self.__commandsUrl}',
-            replyMessageId = await ctx.getMessageId()
+        self.__twitchChatMessenger.send(
+            text = f'ⓘ Commands: {self.__commandsUrl}',
+            twitchChannelId = await ctx.getTwitchChannelId(),
+            replyMessageId = await ctx.getMessageId(),
         )
 
-        self.__timber.log('CommandsChatCommand', f'Handled !commands command for {ctx.getAuthorName()}:{ctx.getAuthorId()} in {user.handle}')
+        self.__timber.log('CommandsChatCommand', f'Handled command for {ctx.getAuthorName()}:{ctx.getAuthorId()} in {user.handle}')
