bff95d705bf63bea480d5d8135bc4cc889a34d6b
Charles Madere <charlesrmadere@gmail.com>
2026-01-06 18:34:24 -0800

    removed many uses of the old `TwitchUiltsInterface` class

--- src/chatCommands/setFuntoonTokenChatCommand.py
+++ src/chatCommands/setFuntoonTokenChatCommand.py
@@ -6,8 +6,8 @@ from ..funtoon.tokens.funtoonTokensRepositoryInterface import FuntoonTokensRepos
 from ..misc import utils as utils
 from ..misc.administratorProviderInterface import AdministratorProviderInterface
 from ..timber.timberInterface import TimberInterface
+from ..twitch.chatMessenger.twitchChatMessengerInterface import TwitchChatMessengerInterface
 from ..twitch.configuration.twitchContext import TwitchContext
-from ..twitch.twitchUtilsInterface import TwitchUtilsInterface
 from ..users.usersRepositoryInterface import UsersRepositoryInterface
 
 
@@ -18,7 +18,7 @@ class SetFuntoonTokenChatCommand(AbsChatCommand):
         administratorProvider: AdministratorProviderInterface,
         funtoonTokensRepository: FuntoonTokensRepositoryInterface,
         timber: TimberInterface,
-        twitchUtils: TwitchUtilsInterface,
+        twitchChatMessenger: TwitchChatMessengerInterface,
         usersRepository: UsersRepositoryInterface,
     ):
         if not isinstance(administratorProvider, AdministratorProviderInterface):
@@ -27,15 +27,15 @@ class SetFuntoonTokenChatCommand(AbsChatCommand):
             raise TypeError(f'funtoonTokensRepository argument is malformed: \"{funtoonTokensRepository}\"')
         elif not isinstance(timber, TimberInterface):
             raise TypeError(f'timber argument is malformed: \"{timber}\"')
-        elif not isinstance(twitchUtils, TwitchUtilsInterface):
-            raise TypeError(f'twitchUtils argument is malformed: \"{twitchUtils}\"')
+        elif not isinstance(twitchChatMessenger, TwitchChatMessengerInterface):
+            raise TypeError(f'twitchChatMessenger argument is malformed: \"{twitchChatMessenger}\"')
         elif not isinstance(usersRepository, UsersRepositoryInterface):
             raise TypeError(f'usersRepository argument is malformed: \"{usersRepository}\"')
 
         self.__administratorProvider: Final[AdministratorProviderInterface] = administratorProvider
         self.__funtoonTokensRepository: Final[FuntoonTokensRepositoryInterface] = funtoonTokensRepository
         self.__timber: Final[TimberInterface] = timber
-        self.__twitchUtils: Final[TwitchUtilsInterface] = twitchUtils
+        self.__twitchChatMessenger: Final[TwitchChatMessengerInterface] = twitchChatMessenger
         self.__usersRepository: Final[UsersRepositoryInterface] = usersRepository
 
     async def handleChatCommand(self, ctx: TwitchContext):
@@ -50,13 +50,21 @@ class SetFuntoonTokenChatCommand(AbsChatCommand):
         splits = utils.getCleanedSplits(ctx.getMessageContent())
         if len(splits) < 2:
             self.__timber.log('SetFuntoonTokenCommand', f'Not enough arguments given by {ctx.getAuthorName()}:{ctx.getAuthorId()} for the !setfuntoontoken command: \"{splits}\"')
-            await self.__twitchUtils.safeSend(ctx, f'⚠ Token argument is necessary for the !setfuntoontoken command. Example: !setfuntoontoken {self.__getRandomTokenStr()}')
+            self.__twitchChatMessenger.send(
+                text = f'⚠ Token argument is necessary for the !setfuntoontoken command. Example: !setfuntoontoken {self.__getRandomTokenStr()}',
+                twitchChannelId = await ctx.getTwitchChannelId(),
+                replyMessageId = await ctx.getMessageId(),
+            )
             return
 
         token: str | None = splits[1]
         if not utils.isValidStr(token):
             self.__timber.log('SetFuntoonTokenCommand', f'Invalid token argument given by {ctx.getAuthorName()}:{ctx.getAuthorId()} for the !setfuntoontoken command: \"{splits}\"')
-            await self.__twitchUtils.safeSend(ctx, f'⚠ Token argument is necessary for the !setfuntoontoken command. Example: !setfuntoontoken {self.__getRandomTokenStr()}')
+            self.__twitchChatMessenger.send(
+                text = f'⚠ Token argument is necessary for the !setfuntoontoken command. Example: !setfuntoontoken {self.__getRandomTokenStr()}',
+                twitchChannelId = await ctx.getTwitchChannelId(),
+                replyMessageId = await ctx.getMessageId(),
+            )
             return
 
         await self.__funtoonTokensRepository.setToken(
@@ -64,8 +72,13 @@ class SetFuntoonTokenChatCommand(AbsChatCommand):
             twitchChannelId = twitchChannelId,
         )
 
-        self.__timber.log('SetFuntoonTokenCommand', f'Handled !setfuntoontoken command for {ctx.getAuthorName()}:{ctx.getAuthorId()} in {user.handle}')
-        await self.__twitchUtils.safeSend(ctx, f'ⓘ Funtoon token has been updated')
+        self.__twitchChatMessenger.send(
+            text = f'ⓘ Funtoon token has been updated',
+            twitchChannelId = await ctx.getTwitchChannelId(),
+            replyMessageId = await ctx.getMessageId(),
+        )
+
+        self.__timber.log('SetFuntoonTokenCommand', f'Handled command for {ctx.getAuthorName()}:{ctx.getAuthorId()} in {user.handle}')
 
     def __getRandomTokenStr(self) -> str:
         randomUuid = str(uuid.uuid4())
