907552baa94d77d9bae8246eb6d69b8341011364
Charles Madere <charlesrmadere@gmail.com>
2026-01-19 15:18:46 +0900

    we now initialize some of the gashapon classes

--- src/chatterInventory/models/gashaponResults/gashaponRewardedGashaponResult.py
+++ src/chatterInventory/models/gashaponResults/gashaponRewardedGashaponResult.py
@@ -11,13 +11,8 @@ from ..chatterItemType import ChatterItemType
 class GashaponRewardedGashaponResult(AbsGashaponResult):
     chatterInventory: ChatterInventoryData
     chatterUserId: str
+    hypeEmote: str
     twitchChannelId: str
-
-    def __getitem__(self, key: ChatterItemType) -> int:
-        if not isinstance(key, ChatterItemType):
-            raise TypeError(f'itemType argument is malformed: \"{key}\"')
-
-        return self.chatterInventory[key]
 
     @property
     def inventory(self) -> frozendict[ChatterItemType, int]:
