2dd2ef82ad05325f5f280d76104052d3d98d5f33
Charles Madere <charlesrmadere@gmail.com>
2026-04-05 17:26:08 -0700

    created new `CutenessMapper` class

---
+++ src/cuteness/mappers/cutenessMapperInterface.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Any
+
+
+class CutenessMapperInterface(ABC):
+
+    @abstractmethod
+    async def parseUtcYearAndMonthString(
+        self,
+        utcYearAndMonthString: str | Any | None,
+    ) -> datetime | None:
+        pass
+
+    @abstractmethod
+    async def requireUtcYearAndMonthString(
+        self,
+        utcYearAndMonthString: str | Any | None,
+    ) -> datetime:
+        pass
--- src/cuteness/repositories/cutenessRepository.py
+++ src/cuteness/repositories/cutenessRepository.py
@@ -5,6 +5,7 @@ from typing import Final
 from frozenlist import FrozenList
 
 from .cutenessRepositoryInterface import CutenessRepositoryInterface
+from ..mappers.cutenessMapperInterface import CutenessMapperInterface
 from ..models.cutenessChampionsResult import CutenessChampionsResult
 from ..models.cutenessHistoryEntry import CutenessHistoryEntry
 from ..models.cutenessHistoryResult import CutenessHistoryResult
@@ -36,12 +37,15 @@ class CutenessRepository(CutenessRepositoryInterface):
     def __init__(
         self,
         backingDatabase: BackingDatabase,
+        cutenessMapper: CutenessMapperInterface,
         cutenessSettings: CutenessSettingsInterface,
         timber: TimberInterface,
         timeZoneRepository: TimeZoneRepositoryInterface,
     ):
         if not isinstance(backingDatabase, BackingDatabase):
             raise TypeError(f'backingDatabase argument is malformed: \"{backingDatabase}\"')
+        elif not isinstance(cutenessMapper, CutenessMapperInterface):
+            raise TypeError(f'cutenessMapper argument is malformed: \"{cutenessMapper}\"')
         elif not isinstance(cutenessSettings, CutenessSettingsInterface):
             raise TypeError(f'cutenessSettings argument is malformed: \"{cutenessSettings}\"')
         elif not isinstance(timber, TimberInterface):
@@ -50,6 +54,7 @@ class CutenessRepository(CutenessRepositoryInterface):
             raise TypeError(f'timeZoneRepository argument is malformed: \"{timeZoneRepository}\"')
 
         self.__backingDatabase: Final[BackingDatabase] = backingDatabase
+        self.__cutenessMapper: Final[CutenessMapperInterface] = cutenessMapper
         self.__cutenessSettings: Final[CutenessSettingsInterface] = cutenessSettings
         self.__timber: Final[TimberInterface] = timber
         self.__timeZoneRepository: Final[TimeZoneRepositoryInterface] = timeZoneRepository
@@ -182,8 +187,8 @@ class CutenessRepository(CutenessRepositoryInterface):
             )
 
         for record in records:
-            cutenessDate = datetime.strptime(record[1], '%Y-%m').replace(
-                tzinfo = self.__timeZoneRepository.getDefault(),
+            cutenessDate = await self.__cutenessMapper.requireUtcYearAndMonthString(
+                utcYearAndMonthString = record[1],
             )
 
             cuteness = int(round(record[0]))
@@ -224,8 +229,8 @@ class CutenessRepository(CutenessRepositoryInterface):
         bestCuteness: CutenessHistoryEntry | None = None
 
         if record is not None and len(record) >= 1:
-            bestCutenessDate = datetime.strptime(record[1], '%Y-%m').replace(
-                tzinfo = self.__timeZoneRepository.getDefault(),
+            bestCutenessDate = await self.__cutenessMapper.requireUtcYearAndMonthString(
+                utcYearAndMonthString = record[1],
             )
 
             # again, this should be impossible here, but let's just be safe
