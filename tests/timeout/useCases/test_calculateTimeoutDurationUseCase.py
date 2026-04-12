37bb3e60e08b4b78bd3cf133ca21fe3efe6b1a70
Charles Madere <charlesrmadere@gmail.com>
2026-04-12 03:28:55 -0700

    slightly weaken aniv timeout scale

--- tests/timeout/useCases/test_calculateTimeoutDurationUseCase.py
+++ tests/timeout/useCases/test_calculateTimeoutDurationUseCase.py
@@ -29,7 +29,7 @@ class TestCalculateTimeoutDurationUseCase:
     @pytest.mark.asyncio
     async def test_invoke_withRandomExponentialTimeoutDuration(self):
         timeoutDuration = RandomExponentialTimeoutDuration(
-            scale = float(9),
+            scale = float(10),
             maximumSeconds = 300,
             minimumSeconds = 30,
         )
