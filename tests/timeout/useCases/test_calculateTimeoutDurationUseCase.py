47cd3971cc8ad4218a239c4ba72ba4115fc94d15
Charles Madere <charlesrmadere@gmail.com>
2026-04-12 03:22:35 -0700

    added more tests of `CalculateTimeoutDurationUseCase`

--- tests/timeout/useCases/test_calculateTimeoutDurationUseCase.py
+++ tests/timeout/useCases/test_calculateTimeoutDurationUseCase.py
@@ -2,7 +2,9 @@ from typing import Final
 
 import pytest
 
+from src.timeout.models.exactTimeoutDuration import ExactTimeoutDuration
 from src.timeout.models.randomExponentialTimeoutDuration import RandomExponentialTimeoutDuration
+from src.timeout.models.randomLinearTimeoutDuration import RandomLinearTimeoutDuration
 from src.timeout.useCases.calculateTimeoutDurationUseCase import CalculateTimeoutDurationUseCase
 from src.timeout.useCases.calculateTimeoutDurationUseCaseInterface import CalculateTimeoutDurationUseCaseInterface
 
@@ -12,9 +14,37 @@ class TestCalculateTimeoutDurationUseCase:
     useCase: Final[CalculateTimeoutDurationUseCaseInterface] = CalculateTimeoutDurationUseCase()
 
     @pytest.mark.asyncio
+    async def test_invoke_withExactTimeoutDuration(self):
+        timeoutDuration = ExactTimeoutDuration(
+            seconds = 30,
+        )
+
+        for _ in range(100):
+            calculatedTimeoutDuration = await self.useCase.invoke(
+                timeoutDuration = timeoutDuration,
+            )
+
+            assert calculatedTimeoutDuration.seconds == timeoutDuration.seconds
+
+    @pytest.mark.asyncio
     async def test_invoke_withRandomExponentialTimeoutDuration(self):
         timeoutDuration = RandomExponentialTimeoutDuration(
             scale = float(9),
+            maximumSeconds = 300,
+            minimumSeconds = 30,
+        )
+
+        for _ in range(100):
+            calculatedTimeoutDuration = await self.useCase.invoke(
+                timeoutDuration = timeoutDuration,
+            )
+
+            assert calculatedTimeoutDuration.seconds >= timeoutDuration.minimumSeconds
+            assert calculatedTimeoutDuration.seconds <= timeoutDuration.maximumSeconds
+
+    @pytest.mark.asyncio
+    async def test_invoke_withRandomLinearTimeoutDuration(self):
+        timeoutDuration = RandomLinearTimeoutDuration(
             maximumSeconds = 300,
             minimumSeconds = 30,
         )
