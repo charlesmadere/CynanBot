6e86d681d863eec2b0c371e97a2a4c1a63255275
Charles Madere <charlesrmadere@gmail.com>
2026-04-15 18:51:15 -0700

    another watch streak TTS message change

--- src/watchStreaks/helper/watchStreaksHelper.py
+++ src/watchStreaks/helper/watchStreaksHelper.py
@@ -96,7 +96,7 @@ class WatchStreaksHelper(WatchStreaksHelperInterface):
             twitchChannel = user.handle,
             twitchChannelId = twitchChannelId,
             ttsEvent = TtsEvent(
-                message = f'{callOutUserName} sup, and thanks for the watch streak of {watchStreak}!',
+                message = f'Thanks {callOutUserName} for the watch streak of {watchStreak}!',
                 twitchChannel = user.handle,
                 twitchChannelId = twitchChannelId,
                 userId = chatterUserId,
