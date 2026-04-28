7b26c55b9d1f2a77695c9bb6f7128029740ce108
Charles Madere <charlesrmadere@gmail.com>
2026-04-28 01:33:45 -0700

    deleted unused `AbsChatCommand` classes

--- src/chatCommands/stubChatCommand.py
+++
@@ -1,9 +0,0 @@
-from .absChatCommand import AbsChatCommand
-from ..twitch.configuration.twitchContext import TwitchContext
-
-
-class StubChatCommand(AbsChatCommand):
-
-    async def handleChatCommand(self, ctx: TwitchContext):
-        # this method is intentionally empty
-        pass
