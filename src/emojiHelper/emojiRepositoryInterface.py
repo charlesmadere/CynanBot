b0d41fbac9ab2aab35720ad1fb04b828c4791182
Charles Madere <charlesrmadere@gmail.com>
2026-06-07 03:28:36 -0700

    added easy methods for fetching particular emoji categories

--- src/emojiHelper/emojiRepositoryInterface.py
+++ src/emojiHelper/emojiRepositoryInterface.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
+from typing import Collection
 
 from .emojiData import EmojiData
 
 
 class EmojiRepositoryInterface(ABC):
+
+    @abstractmethod
+    async def fetchEmojiCategory(self, category: str) -> Collection[EmojiData]:
+        pass
 
     @abstractmethod
     async def fetchEmojiData(self, emoji: str | None) -> EmojiData | None:
