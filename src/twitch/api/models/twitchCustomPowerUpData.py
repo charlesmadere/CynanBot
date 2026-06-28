64092732549c174120717b1cde883bad5044b5fd
Charles Madere <charlesrmadere@gmail.com>
2026-06-28 03:27:57 -0700

    added some additional Twitch custom power up data

--- src/twitch/api/models/twitchCustomPowerUpData.py
+++ src/twitch/api/models/twitchCustomPowerUpData.py
@@ -3,5 +3,7 @@ from dataclasses import dataclass
 
 @dataclass(frozen = True, slots = True)
 class TwitchCustomPowerUpData:
+    bits: int | None
+    prompt: str | None
     rewardId: str
     title: str
